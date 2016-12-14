dce53f2d6ba98dfad1c20cdfe7311d72a0e8d800
Andrew Baker <abaker@element84.com>
2016-12-14 16:05:09 -0500

    adding basic http auth to all pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  http_basic_authenticate_with name: ENV["HTTP_AUTH_NAME"], password: ENV["HTTP_AUTH_PASS"]
 
 
   #saving error from CanCan for users going beyond allowed pages
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  
   Rails.application.routes.draw do
       devise_for :users, controllers: {
         registrations: 'users/registrations'
