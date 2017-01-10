99d886ff247ab5fa36848ebd0d5bd00d86ba0686
Andrew Baker <abaker@element84.com>
2017-01-10 14:17:17 -0500

    moving logic out of controller into model and finally! adding aws templates

--- app/models/collection_record.rb
+++ app/models/collection_record.rb
@@ -5,4 +5,43 @@ class CollectionRecord < ActiveRecord::Base
   has_many :collection_reviews
 
   has_many :users, through: :collection_reviews   
+
+  def self.ingested?(concept_id, revision_id)
+    CollectionRecord.where(concept_id: concept_id, version_id: revision_id).any?
+  end
+
+  def self.ingest(concept_id)
+    collection_data = Curation.collection_data(concept_id)
+    already_ingested = CollectionRecord.ingested?(concept_id, revision_id)
+    granule_results = Curation.granule_list_from_collection(concept_id)
+
+    concept_id = collection_data["concept_id"]
+    shortname = collection_data["Collection"]["ShortName"]
+    version_id = collection_data["revision_id"]
+
+    unless already_ingested
+      record = CollectionRecord.new
+      record.concept_id = concept_id
+      record.short_name = shortname
+      record.version_id = version_id
+      record.closed = false
+      record.rawJSON = collection_data.to_json
+      record.save
+
+      ingest_record = CollectionIngest.new
+      ingest_record.collection_record_id = record.id
+      ingest_record.user_id = current_user.id
+      ingest_record.date_ingested = DateTime.now
+      ingest_record.save
+
+      if granule_results["hits"].to_i == 0
+      else
+      end
+
+      return true
+    else
+      return false
+    end 
+  end
+
 end
