9cea3526ab738c82212a7279377c3a931cb081e7
Andrew Baker <abaker@element84.com>
2017-01-12 12:53:21 -0500

    setting the base for comment creation and automated script reviews.  Automated review user is -1, could make that a reserved or application wide VAR

--- app/controllers/collection_controller.rb
+++ app/controllers/collection_controller.rb
@@ -10,4 +10,25 @@ class CollectionController < ApplicationController
     @collection_records = CollectionRecord.where(concept_id: @concept_id).order(:version_id).reverse_order
   end
 
+  def record_review
+    @concept_id = params["concept_id"]
+    @revision_id = params["revision_id"]
+
+    if !(@concept_id || @revision_id)
+      flash[:error] = "Missing the concept_id or revision_id to Locate review"
+      redirect_to curation_home_path
+    end
+
+    @collection_record = CollectionRecord.where(concept_id: @concept_id, version_id: @revision_id).first
+    @record_comments = @collection_record.collection_comments
+    @user_record = @record_comments.select { |comment| comment.user_id == current_user.id }
+
+    if @user_record.empty?
+      @user_record = @collection_record.add_comment(current_user)
+    else
+      @user_record = @user_record.first
+    end
+
+  end
+
 end
