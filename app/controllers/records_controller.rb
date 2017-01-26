b5a979eccf1934ead85f2d6cb2f7fc58324570fb
Andrew Baker <abaker@element84.com>
2017-01-26 11:01:20 -0500

    changing some links to provide pipeline to see styling

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,4 +1,7 @@
 class RecordsController < ApplicationController
   def show
+    record = Record.find_by id: params["id"]
+    @bubble_data = record.section_bubble_data(Record::COLLECTION_INFORMATION_FIELDS)
   end
+
 end
