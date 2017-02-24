714b3be0d20e371318a4f869ce9a57ae5a01db25
Andrew Baker <abaker@element84.com>
2017-02-24 15:24:18 -0500

    reorganize testing code into method by method format

--- test/controllers/collections_controller_test.rb
+++ test/controllers/collections_controller_test.rb
@@ -1,20 +1,29 @@
 class CollectionsControllerTest < ActionController::TestCase
-  it "loads the correct collection on show" do
-    @tester = User.find_by_email("abaker@element84.com")
-    sign_in(@tester)
+    describe "GET #show" do
+      context "when successful" do
+        it "loads the correct collection on show" do
+            @tester = User.find_by_email("abaker@element84.com")
+            sign_in(@tester)
 
-    get :show, id: 1, concept_id: "C1000000020-LANCEAMSR2"
+            get :show, id: 1, concept_id: "C1000000020-LANCEAMSR2"
 
-    collection_records = assigns(:collection_records)
-    assert_equal(collection_records.length, 1)
-
-
-    #redirects no concept_id
-    get :show, id: 1
-    assert_equal(response.code, "302")
-
-    #redirects no collection found
-    get :show, id: 1, concept_id: "xyz"
-    assert_equal(response.code, "302")
-  end
+            collection_records = assigns(:collection_records)
+            assert_equal(collection_records.length, 1)
+        end
+      end
+      context "when no concept id is provided" do
+        it "redirects" do
+            #redirects no concept_id
+            get :show, id: 1
+            assert_equal(response.code, "302")
+        end
+      end
+      context "when no collection is found" do
+        it "redirects" do
+            #redirects no collection found
+            get :show, id: 1, concept_id: "xyz"
+            assert_equal(response.code, "302")
+        end
+      end
+    end
 end
