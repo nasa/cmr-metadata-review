d472acced45c30b049523015d2efb20f2f201ec5
Andrew Baker <abaker@element84.com>
2017-02-24 16:20:22 -0500

    updating more test to method based format

--- test/controllers/collections_controller_test.rb
+++ test/controllers/collections_controller_test.rb
@@ -1,6 +1,5 @@
 class CollectionsControllerTest < ActionController::TestCase
     describe "GET #show" do
-      context "when successful" do
         it "loads the correct collection on show" do
             @tester = User.find_by_email("abaker@element84.com")
             sign_in(@tester)
@@ -9,21 +8,19 @@ class CollectionsControllerTest < ActionController::TestCase
             collection_records = assigns(:collection_records)
             assert_equal(collection_records.length, 1)
         end
-      end
-      context "when no concept id is provided" do
-        it "redirects" do
+
+        it "redirects when no concept id is provided" do
             #redirects no concept_id
             get :show, id: 1
             assert_equal(response.code, "302")
         end
-      end
-      context "when no collection is found" do
-        it "redirects" do
+
+        it "redirects when no collection is found" do
             #redirects no collection found
             get :show, id: 1, concept_id: "xyz"
             assert_equal(response.code, "302")
         end
-      end
+
     end
 
 
