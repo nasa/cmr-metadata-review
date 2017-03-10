6efe32a35f498fb2b30311c94e60c763beffcd43
Andrew Baker <abaker@element84.com>
2017-03-10 12:14:48 -0500

    organizing tests into by method groupings

--- test/models/datable_test.rb
+++ test/models/datable_test.rb
@@ -2,15 +2,15 @@ require 'test_helper'
 
 class DatableTest < ActiveSupport::TestCase
 
-  it "fills in datable values if values are missing" do
-    record = Record.find_by id: 1
-
-    #This is to test the safety method of generating JSON for a datable object with no data.
-    #should be unreachable due to initialization in the get_[data_type] methods
-    #but still wanted to keep and test directly
-    colors = Color.new(record: record)
-    values = colors.values
-    assert_equal(values["ShortName"], "")
-
+  describe "values" do
+    it "fills in datable values if values are missing" do
+      record = Record.find_by id: 1
+      #This is to test the safety method of generating JSON for a datable object with no data.
+      #should be unreachable due to initialization in the get_[data_type] methods
+      #but still wanted to keep and test directly
+      colors = Color.new(record: record)
+      values = colors.values
+      assert_equal(values["ShortName"], "")
+    end
   end
 end
