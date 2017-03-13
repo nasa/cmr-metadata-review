d524918cc72a88c1ab752051e741a208582c9989
Andrew Baker <abaker@element84.com>
2017-03-13 14:25:20 -0400

    finishing the collection checker script

--- lib/CollectionChecker.py
+++ lib/CollectionChecker.py
@@ -216,7 +216,7 @@ class Checker():
             # except KeyError:
             #     result += 'np' + ', '
             # ================
-            resultFields += 'SpatialKeywords/Keyword' + ', '
+            resultFields += 'SpatialKeywords/Keyword' + ', , , , , , , '
             try:
                 result += self.checkSpatialKey(metadata['SpatialKeywords']['Keyword']) + ', , , , , , , '
             except KeyError:
@@ -237,31 +237,39 @@ class Checker():
             except KeyError:
                 result += 'np' + ', '
             # ================
+            resultFields += 'Temporal/SingleDateTime/BeginningDateTime' + ', '
             try:
                 result += self.checkBeginDateTime(metadata['Temporal']['RangeDateTime']['BeginningDateTime']) + ', '
             except KeyError:
                 result += 'np' + ', '
             # ================
+            resultFields += 'Temporal/RangeDateTime/EndingDateTime' + ', , , , , , , , '   
             try:
                 result += self.checkEndDateTime(metadata['Temporal']['RangeDateTime']['EndingDateTime']) + ', , , , , , , , '    
             except KeyError:
                 result += 'np' + ', , , , , , , , '    
             # ================
+            resultFields += 'Contacts/Contact/Role' + ', , , , , , , , , , , '  
             try:
                 result += self.checkContactRole(metadata['Contacts']['Contact']['Role']) + ', , , , , , , , , , , '
             except KeyError:
                 result += 'np' + ', , , , , , , , , , , '
             # ================
-            try:
+            try:                
                 result += self.checkContactEmail(metadata['Contacts']['Contact']['OrganizationEmails']['Email'], 1) + ', , , , , '
+                resultFields += 'Contacts/Contact/OrganizationEmails/Email' + ', , , , , '
             except KeyError:
                 if metadata['Contacts']['Contact']['OrganizationEmails'] != None:
                     length = len(metadata['Contacts']['Contact']['OrganizationEmails'])
                     result += self.checkContactEmail(metadata['Contacts']['Contact']['OrganizationEmails'], length) + ', , , , , '
+                    resultFields += 'Contacts/Contact/OrganizationEmails/Email' + ', , , , , '
                 else:
+                    resultFields += 'Contacts/Contact/OrganizationEmails/Email' + ', , , , , '
                     result += 'np' + ', , , , , '
             # ================
             ScienceKeywords = fetchAllSciKeyWords()
+
+            resultFields += 'ScienceKeywords/ScienceKeyword/CategoryKeyword' + ', '
             try:
                 result += self.checkSciKeyCategory(metadata['ScienceKeywords']['ScienceKeyword']['CategoryKeyword'], 1, ScienceKeywords) + ', '
             except TypeError:
@@ -273,6 +281,8 @@ class Checker():
             except KeyError:
                 result += 'Provide at least one science category keyword for this dataset. This is a required field.' + ', '
             # ================
+
+            resultFields += 'ScienceKeywords/ScienceKeyword/TopicKeyword' + ', '
             try:
                 result += self.checkSciKeyTopic(metadata['ScienceKeywords']['ScienceKeyword']['TopicKeyword'], 1, ScienceKeywords) + ', '
             except TypeError:
@@ -284,6 +294,8 @@ class Checker():
             except KeyError:
                 result += 'Provide at least one science topic keyword for this dataset. This is a required field.' + ', '
             # ================
+
+            resultFields += 'ScienceKeywords/ScienceKeyword/TermKeyword' + ', '
             try:
                 result += self.checkSciKeyTerm(metadata['ScienceKeywords']['ScienceKeyword']['TermKeyword'], 1, ScienceKeywords) + ', '
             except TypeError:
@@ -295,6 +307,8 @@ class Checker():
             except KeyError:
                 result += 'Provide at least one science term keyword for this dataset. This is a required field.' + ', '
             # ================
+
+            resultFields += 'ScienceKeywords/ScienceKeyword/VariableLevel1Keyword/Value' + ', '
             try:
                 result += self.checkSciKeyVarL1(metadata['ScienceKeywords']['ScienceKeyword']['VariableLevel1Keyword']['Value'], 1, ScienceKeywords) + ', '
             except TypeError:
@@ -306,6 +320,8 @@ class Checker():
             except KeyError:
                 result += 'np' + ', '
             # ================
+
+            resultFields += 'ScienceKeywords/ScienceKeyword/VariableLevel1Keyword/VariableLevel2Keyword/Value' + ', '
             try:
                 result += self.checkSciKeyVarL2(metadata['ScienceKeywords']['ScienceKeyword']['VariableLevel1Keyword']['VariableLevel2Keyword']['Value'], 1, ScienceKeywords) + ', '
             except TypeError:
@@ -317,6 +333,8 @@ class Checker():
             except KeyError:
                 result += 'np' + ', '
             # ================
+
+            resultFields += 'ScienceKeywords/ScienceKeyword/VariableLevel1Keyword/VariableLevel2Keyword/VariableLevel3Keyword/Value' + ', , '
             try:
                 result += self.checkSciKeyVarL3(metadata['ScienceKeywords']['ScienceKeyword']['VariableLevel1Keyword']['VariableLevel2Keyword']['VariableLevel3Keyword']['Value'], 1, ScienceKeywords) + ', , '
             except TypeError:
@@ -330,6 +348,9 @@ class Checker():
             # ================
             platforms = fetchAllPlatforms()
             PlatformShortName = []
+
+
+            resultFields += 'Platforms/Platform/ShortName' + ', '
             try:
                 result += self.checkPlatformShortName(metadata['Platforms']['Platform']['ShortName'], 1, platforms) + ', '
                 PlatformShortName += metadata['Platforms']['Platform']['ShortName']
@@ -344,6 +365,9 @@ class Checker():
             except KeyError:
                 result += 'Provide at least one platform for this dataset. This is a required field.' + ', '
             # ================
+
+
+            resultFields += 'Platforms/Platform/LongName' + ', '
             try:
                 result += self.checkPlatformLongName(metadata['Platforms']['Platform']['LongName'], 1, PlatformShortName, platforms) + ', '
             except TypeError:
@@ -355,6 +379,8 @@ class Checker():
             except KeyError:
                 result += 'Recommend adding a platform long name, if applicable.' + ', '
             # ================
+
+            resultFields += 'Platforms/Platform/Type' + ', , , , , , '
             try:
                 metadata['Platforms']['Platform']['Type']
                 result += self.checkPlatformType(metadata['Platforms']['Platform']['Type'], 1, platforms) + ', , , , , , '
@@ -369,6 +395,8 @@ class Checker():
             # ================
             instruments = fetchAllInstrs()
             sensorShortResult = ''
+
+            resultField += 'Platforms/Platform/Instruments/Instrument/ShortName' + ','
             try:
                 metadata['Platforms']['Platform']['ShortName']
                 platform_num = 1
@@ -385,6 +413,8 @@ class Checker():
                 result += 'Provide at least one relevant instrument for this dataset. This is a required field.' + ', '
             # ================
             sensorLongResult = ''
+
+            resultField += 'Platforms/Platform/Instruments/Instrument/LongName' + ', , , , , , , , '
             try:
                 metadata['Platforms']['Platform']['LongName']
                 platform_num = 1
@@ -400,6 +430,8 @@ class Checker():
             except KeyError:
                 result += 'Recommend providing an instrument long name; since many instrument long names are comprised of acronyms.' + ', , , , , , , , '
             # ================
+
+
             if len(sensorShortResult) == 0:
                 result += 'np , '
             else:
@@ -410,6 +442,8 @@ class Checker():
             else:
                 result += sensorLongResult + ', , , , , , , , , , , , , , , , , , , , , , '
             # ================
+
+            resultFields += 'Campaigns/Campaign/ShortName' + ', '
             try:
                 result += self.checkCampaignShortName(metadata['Campaigns']['Campaign']['ShortName'], 1) + ', '
             except TypeError:
@@ -418,6 +452,8 @@ class Checker():
             except KeyError:
                 result += 'np' + ', '
             # ================
+
+            resultFields += 'Campaigns/Campaign/LongName' + ', '
             try:
                 result += self.checkCampaignLongName(metadata['Campaigns']['Campaign']['LongName'], 1) + ', '
             except TypeError:
@@ -426,6 +462,8 @@ class Checker():
             except KeyError:
                 result += 'np' + ', '
             # ================
+
+            resultFields += 'Campaigns/Campaign/StartDate' + ', '            
             try:
                 result += self.checkCampaignStartDate(metadata['Campaigns']['Campaign']['StartDate'], 1) + ', '
             except TypeError:
@@ -434,6 +472,8 @@ class Checker():
             except KeyError:
                 result += 'np' + ', '
             # ================
+
+            resultFields += 'Campaigns/Campaign/EndDate' + ', , , , , , '
             try:
                 result += self.checkCampaignEndDate(metadata['Campaigns']['Campaign']['EndDate'], 1) + ', , , , , , '
             except TypeError:
@@ -442,6 +482,8 @@ class Checker():
             except KeyError:
                 result += 'np' + ', , , , , , '
             # ================
+
+            resultFields += 'OnlineAccessURLs/OnlineAccessURL/URL' + ','
             try:
                 result += self.checkOnlineAccessURL(metadata['OnlineAccessURLs']['OnlineAccessURL']['URL'], 1, metadata['ArchiveCenter']) + ','
             except TypeError:
@@ -453,6 +495,8 @@ class Checker():
             except KeyError:
                 result += 'np' + ','
             # ================
+
+            resultFields += 'OnlineAccessURLs/OnlineAccessURL/Description' + ', ,'
             try:
                 result += self.checkOnlineURLDesc(metadata['OnlineAccessURLs']['OnlineAccessURL']['Description'], 1) + ', ,'
             except TypeError:
@@ -464,6 +508,9 @@ class Checker():
             except KeyError:
                 result += 'Recommend providing descriptions for all Online Access URLs.' + ', ,'
             # ================
+
+
+            resultFields += 'OnlineResources/OnlineResource/URL' + ','
             try:
                 result += self.checkOnlineResourceURL(metadata['OnlineResources']['OnlineResource']['URL'], 1) + ','
             except TypeError:
@@ -475,6 +522,8 @@ class Checker():
             except KeyError:
                 result += 'np' + ','
             # ================
+
+            resultFields += 'OnlineResources/OnlineResource/Description' + ','
             try:
                 result += self.checkOnlineResourceURLDesc(metadata['OnlineResources']['OnlineResource']['Description'], 1) + ','
             except TypeError:
@@ -486,6 +535,8 @@ class Checker():
             except KeyError:
                 result += 'np' + ','
             # ================
+
+            resultFields += 'OnlineResources/OnlineResource/Type' + ', , , , , , , , '
             try:
                 result += self.checkOnlineResourceType(metadata['OnlineResources']['OnlineResource']['Type'], 1) + ', , , , , , , , '
             except TypeError:
@@ -499,6 +550,8 @@ class Checker():
             # ================
             # result += self.checkSpatialHorGeoCoor(metadata['Spatial']['HorizontalSpatialDomain']['Geometry']['CoordinateSystem']) + ', '
             # ================
+
+            resultFields += 'Spatial/HorizontalSpatialDomain/Geometry/BoundingRectangle' + ', , , , , , , , '
             result += self.checkBoundingRectangle(metadata['Spatial']['HorizontalSpatialDomain']['Geometry']['BoundingRectangle']) + ', , , , , , , , '
             # # ================
             # try:
@@ -1620,6 +1673,13 @@ x = Checker()
 result, resultFields = x.checkAll(sys.argv[1])
 # result, resultFields = x.checkAll("{\"ShortName\":\"CIESIN_SEDAC_NRMI_NRPCHI15\",\"VersionId\":\"2015.00\",\"InsertTime\":\"2016-11-02T00:00:00.000Z\",\"LastUpdate\":\"2016-11-02T00:00:00.000Z\",\"LongName\":\"Natural Resource Protection and Child Health Indicators, 2015 Release\",\"DataSetId\":\"Natural Resource Protection and Child Health Indicators, 2015 Release\",\"Description\":\"The Natural Resource Protection and Child Health Indicators, 2015 Release, are produced in support of the U.S. Millennium Challenge Corporation as selection criteria for funding eligibility. These indicators are successors to the Natural Resource Management Index (NRMI), which was produced from 2006 to 2011 and was based on the same underlying data. Like the NRMI, the Natural Resource Protection Indicator (NRPI) and Child Health Indicator (CHI) are based on proximity-to-target scores ranging from 0 to 100 (at target). The NRPI covers 221 countries and is calculated based on the weighted average percentage of biomes under protected status. The CHI is a composite index for 188 countries derived from the average of three proximity-to-target scores for access to improved sanitation, access to improved water, and child mortality. The 2015 release includes a consistent time series of NRPIs and CHIs for 2010 to 2015.\",\"CollectionDataType\":\"SCIENCE_QUALITY\",\"Orderable\":\"true\",\"Visible\":\"true\",\"RevisionDate\":\"2016-11-02T00:00:00.000Z\",\"ArchiveCenter\":\"SEDAC\",\"CollectionState\":\"Completed\",\"SpatialKeywords\":{\"Keyword\":[\"AFRICA\",\"ALGERIA\",\"ASIA\",\"AUSTRALIA\",\"BHUTAN\",\"BOTSWANA\",\"BURMA\",\"CAMBODIA\",\"CAMEROON\",\"CANADA\",\"CAPE VERDE\",\"CAYMAN ISLANDS\",\"CENTRAL AFRICAN REPUBLIC\",\"CHAD\",\"CHILE\",\"CHINA\",\"COLOMBIA\",\"COMOROS\",\"CONGO\",\"CONGO, DEMOCRATIC REPUBLIC\",\"COOK ISLANDS\",\"COSTA RICA\",\"COTE D'IVOIRE\",\"CROATIA\",\"CUBA\",\"CURACAO\",\"CYPRUS\",\"CZECH REPUBLIC\",\"DENMARK\",\"DJIBOUTI\",\"DOMINICA\",\"DOMINICAN REPUBLIC\",\"ECUADOR\",\"EGYPT\",\"EL SALVADOR\",\"EQUATORIAL\",\"EQUATORIAL GUINEA\",\"ERITREA\",\"ESTONIA\",\"ETHIOPIA\",\"EUROPE\",\"FAEROE ISLANDS\",\"FALKLAND ISLANDS\",\"FIJI\",\"FINLAND\",\"FRANCE\",\"FRENCH GUIANA\",\"FRENCH POLYNESIA\",\"GABON\",\"GAMBIA\",\"GEORGIA\",\"GERMANY\",\"GHANA\",\"GIBRALTAR\",\"GLOBAL\",\"GREECE\",\"GREENLAND\",\"GRENADA\",\"GUADELOUPE\",\"GUAM\",\"GUATEMALA\",\"GUINEA\",\"GUINEA-BISSAU\",\"GUYANA\",\"HAITI\",\"HONDURAS\",\"HONG KONG\",\"HUNGARY\",\"ICELAND\",\"INDIA\",\"INDONESIA\",\"IRAN\",\"IRAQ\",\"IRELAND\",\"ISLE OF MAN\",\"ISRAEL\",\"ITALY\",\"JAMAICA\",\"JAPAN\",\"JORDAN\",\"KAZAKHSTAN\",\"KENYA\",\"KIRIBATI\",\"KOSOVO\",\"KUWAIT\",\"KYRGYZSTAN\",\"LAO PEOPLE'S DEMOCRATIC REPUBLIC\",\"LATVIA\",\"LEBANON\",\"LESOTHO\",\"LIBERIA\",\"LIBYA\",\"LIECHTENSTEIN\",\"LITHUANIA\",\"LUXEMBOURG\",\"MACAU\",\"MACEDONIA\",\"MADAGASCAR\",\"MALAWI\",\"MALAYSIA\",\"MALDIVES\",\"MALI\",\"MALTA\",\"MARSHALL ISLANDS\",\"MARTINIQUE\",\"MAURITANIA\",\"MAURITIUS\",\"MAYOTTE\",\"MEXICO\",\"MICRONESIA\",\"MID-LATITUDE\",\"MOLDOVA\",\"MONACO\",\"MONGOLIA\",\"MONTENEGRO\",\"MONTSERRAT\",\"MOROCCO\",\"MOZAMBIQUE\",\"NAMIBIA\",\"NAURU\",\"NEPAL\",\"NETHERLANDS\",\"NEW CALEDONIA\",\"NEW ZEALAND\",\"NICARAGUA\",\"NIGER\",\"NIGERIA\",\"NIUE\",\"NORFOLK ISLAND\",\"NORTH AMERICA\",\"NORTH KOREA\",\"NORTHERN MARIANA ISLANDS\",\"NORWAY\",\"OMAN\",\"PAKISTAN\",\"PALAU\",\"PALESTINE\",\"PANAMA\",\"PAPUA NEW GUINEA\",\"PARAGUAY\",\"PERU\",\"PHILIPPINES\",\"PITCAIRN ISLANDS\",\"POLAND\",\"POLAR\",\"PORTUGAL\",\"PUERTO RICO\",\"QATAR\",\"REUNION\",\"ROMANIA\",\"RUSSIAN FEDERATION\",\"RWANDA\",\"SAMOA\",\"SAN MARINO\",\"SAO TOME AND PRINCIPE\",\"SAUDI ARABIA\",\"SENEGAL\",\"SERBIA\",\"SEYCHELLES\",\"SIERRA LEONE\",\"SINGAPORE\",\"SLOVAKIA\",\"SLOVENIA\",\"SOLOMON ISLANDS\",\"SOMALIA\",\"SOUTH AFRICA\",\"SOUTH AMERICA\",\"SOUTH KOREA\",\"SOUTH SUDAN\",\"SPAIN\",\"SRI LANKA\",\"ST HELENA\",\"ST KITTS AND NEVIS\",\"ST LUCIA\",\"ST MAARTEN\",\"ST MARTIN\",\"ST PIERRE AND MIQUELON\",\"ST VINCENT AND THE GRENADINES\",\"SUDAN\",\"SURINAME\",\"SVALBARD AND JAN MAYEN\",\"SWAZILAND\",\"SWEDEN\",\"SWITZERLAND\",\"SYRIAN ARAB REPUBLIC\",\"TAIWAN\",\"TAJIKISTAN\",\"TANZANIA\",\"THAILAND\",\"TIMOR-LESTE\",\"TOGO\",\"TOKELAU\",\"TONGA\",\"TRINIDAD AND TOBAGO\",\"TUNISIA\",\"TURKEY\",\"TURKMENISTAN\",\"TURKS AND CAICOS ISLANDS\",\"TUVALU\",\"UGANDA\",\"UKRAINE\",\"UNITED KINGDOM\",\"UNITED STATES OF AMERICA\",\"URUGUAY\",\"UZBEKISTAN\",\"VANUATU\",\"VENEZUELA\",\"VIETNAM\",\"VIRGIN ISLANDS\",\"WALLIS AND FUTUNA ISLANDS\",\"WESTERN SAHARA\",\"YEMEN\",\"ZAMBIA\",\"ZIMBABWE\"]},\"Temporal\":{\"RangeDateTime\":{\"BeginningDateTime\":\"2010-01-01T00:00:00.000Z\",\"EndingDateTime\":\"2015-12-31T23:59:59.999Z\"}},\"Contacts\":{\"Contact\":[{\"Role\":\"METADATA AUTHOR\",\"OrganizationEmails\":{\"Email\":\"metadata@ciesin.columbia.edu\"},\"ContactPersons\":{\"ContactPerson\":{\"FirstName\":\"unknown\",\"LastName\":\"CIESIN METADATA ADMINISTRATION\"}}},{\"Role\":\"TECHNICAL CONTACT\",\"OrganizationEmails\":{\"Email\":\"ciesin.info@ciesin.columbia.edu\"},\"ContactPersons\":{\"ContactPerson\":{\"FirstName\":\"unknown\",\"LastName\":\"SEDAC USER SERVICES\"}}}]},\"ScienceKeywords\":{\"ScienceKeyword\":[{\"CategoryKeyword\":\"EARTH SCIENCE\",\"TopicKeyword\":\"BIOSPHERE\",\"TermKeyword\":\"ECOLOGICAL DYNAMICS\",\"VariableLevel1Keyword\":{\"Value\":\"COMMUNITY DYNAMICS\",\"VariableLevel2Keyword\":{\"Value\":\"BIODIVERSITY FUNCTIONS\"}}},{\"CategoryKeyword\":\"EARTH SCIENCE\",\"TopicKeyword\":\"BIOSPHERE\",\"TermKeyword\":\"ECOSYSTEMS\",\"VariableLevel1Keyword\":{\"Value\":\"TERRESTRIAL ECOSYSTEMS\",\"VariableLevel2Keyword\":{\"Value\":\"ALPINE/TUNDRA\",\"VariableLevel3Keyword\":\"ALPINE TUNDRA\"}}},{\"CategoryKeyword\":\"EARTH SCIENCE\",\"TopicKeyword\":\"BIOSPHERE\",\"TermKeyword\":\"ECOSYSTEMS\",\"VariableLevel1Keyword\":{\"Value\":\"TERRESTRIAL ECOSYSTEMS\",\"VariableLevel2Keyword\":{\"Value\":\"FORESTS\"}}},{\"CategoryKeyword\":\"EARTH SCIENCE\",\"TopicKeyword\":\"HUMAN DIMENSIONS\",\"TermKeyword\":\"ENVIRONMENTAL IMPACTS\",\"VariableLevel1Keyword\":{\"Value\":\"CONSERVATION\"}},{\"CategoryKeyword\":\"EARTH SCIENCE\",\"TopicKeyword\":\"HUMAN DIMENSIONS\",\"TermKeyword\":\"SUSTAINABILITY\",\"VariableLevel1Keyword\":{\"Value\":\"ENVIRONMENTAL SUSTAINABILITY\"}}]},\"Platforms\":{\"Platform\":{\"ShortName\":\"NOT APPLICABLE\",\"LongName\":null,\"Type\":\"Not applicable\",\"Instruments\":{\"Instrument\":{\"ShortName\":\"NOT APPLICABLE\"}}}},\"Campaigns\":{\"Campaign\":{\"ShortName\":\"NRMI\",\"LongName\":\"Natural Resources Management Index\"}},\"OnlineAccessURLs\":{\"OnlineAccessURL\":{\"URL\":\"http://sedac.ciesin.columbia.edu/data/set/nrmi-natural-resource-protection-child-health-indicators-2015/data-download\",\"URLDescription\":\"Data landing page\"}},\"Spatial\":{\"HorizontalSpatialDomain\":{\"Geometry\":{\"CoordinateSystem\":\"CARTESIAN\",\"BoundingRectangle\":{\"WestBoundingCoordinate\":\"-180\",\"NorthBoundingCoordinate\":\"90\",\"EastBoundingCoordinate\":\"180\",\"SouthBoundingCoordinate\":\"-55\"}}},\"GranuleSpatialRepresentation\":\"CARTESIAN\"}}")
 print("result")
-print(result)
+if result:
+    print(result)
+else
+    print("none\n")    
 print("resultFields")
-print(resultFields)
+# print(resultFields)
+if resultFields:
+    print(resultFields)
+else
+    print("none\n")  
