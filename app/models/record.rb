1c0f2fe0b81524186a54980c89389fb1766ddd07
Andrew Baker <abaker@element84.com>
2017-03-31 17:02:50 -0400

    fixing syntax error

--- app/models/record.rb
+++ app/models/record.rb
@@ -116,23 +116,22 @@ class Record < ActiveRecord::Base
   # The script is run through a shell command which accesses the python scripts in the /lib directory.       
 
   def evaluate_script(raw_data = nil)
-      if raw_data.nil?
-        raw_data = get_raw_data
-      end
-      #escaping json for passing to python
-      collection_json = raw_data.to_json.gsub("\"", "\\\"")
-      #running collection script in python
-      #W option to silence warnings
-      if self.is_collection?  
-        script_results = `python -W ignore lib/CollectionChecker.py "#{collection_json}"  `
-      else
-        script_results = `python -W ignore lib/GranuleChecker.py "#{collection_json}"`
-      end
-
-      comment_hash = JSON.parse(script_results)
-      comment_hash = Record.format_script_comments(comment_hash, self.values)
-      comment_hash
+    if raw_data.nil?
+      raw_data = get_raw_data
     end
+    #escaping json for passing to python
+    collection_json = raw_data.to_json.gsub("\"", "\\\"")
+    #running collection script in python
+    #W option to silence warnings
+    if self.is_collection?  
+      script_results = `python -W ignore lib/CollectionChecker.py "#{collection_json}"  `
+    else
+      script_results = `python -W ignore lib/GranuleChecker.py "#{collection_json}"`
+    end
+
+    comment_hash = JSON.parse(script_results)
+    comment_hash = Record.format_script_comments(comment_hash, self.values)
+    comment_hash
   end
 
   def get_raw_data
