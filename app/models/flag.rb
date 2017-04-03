21883684922f2b0b33fba2c8d1874a6662819ce0
Andrew Baker <abaker@element84.com>
2017-04-03 15:11:18 -0400

    first update of all the changes from other branch, need to fix tests

--- app/models/flag.rb
+++
@@ -1,9 +0,0 @@
-# ActiveRecord class used to store lists of flags related to a record review.     
-# Data is stored using the RecordData model as an attribute of Flag.
-
-class Flag < ActiveRecord::Base
-  include Datable
-  
-  belongs_to :record
-  has_one :record_data, :as => :datable
-end
--- app/models/recommendation.rb
+++
@@ -1,9 +0,0 @@
-# ActiveRecord class used to store recommendation strings for record review fields.     
-# Data is stored using the RecordData model as an attribute of Recommendation.
-
-class Recommendation < ActiveRecord::Base
-  include Datable
-  
-  belongs_to :record
-  has_one :record_data, :as => :datable
-end
