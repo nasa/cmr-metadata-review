5836fcc2abfbba0da590ca2c23b24d2acec1b42f
Andrew Baker <abaker@element84.com>
2017-04-24 12:12:26 -0400

    adding documentation

--- app/models/records_update_lock.rb
+++ app/models/records_update_lock.rb
@@ -8,6 +8,14 @@ class RecordsUpdateLock < ActiveRecord::Base
     return self.last_update
   end
 
+
+  # ====Params   
+  # None
+  # ====Returns
+  # String 
+  # ==== Method
+  # Takes the raw UTC last_update attribute and returns a string version     
+  # of the dateTime in EST Timezone, "03/09/2017 at 4:15pm"
   def self.formatted_last_update
     last_update = (RecordsUpdateLock.find_by id: 1).get_last_update
     return last_update.in_time_zone("Eastern Time (US & Canada)").strftime("%m/%d/%Y at %I:%M%p")
