bfa0c45516ef001705edb25ac4da0b467dd62164
Andrew Baker <abaker@element84.com>
2017-05-26 13:44:08 -0400

    beginning to clean reports

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -16,4 +16,20 @@ module ReportsHelper
     return ((font_range/error_range) * item_difference + min_font).to_s     
   end
 
+  def list_past_months
+    @review_month_counts = []
+    #this should be optimized to bucket all the reviews in one run through
+    [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0].each do |month_count|
+      total_count = (Review.all.select { |review| 
+                                            if review.review_completion_date
+                                              (DateTime.now.month - review.review_completion_date.month) >= month_count
+                                            else
+                                              false 
+                                            end
+                                        }).count
+      @review_month_counts.push(total_count)
+    end
+    @review_month_counts
+  end
+
 end
