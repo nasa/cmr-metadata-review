7996be6a9f03a885c12d0fa9af0027721f7aad2d
Andrew Baker <abaker@element84.com>
2017-05-30 10:04:16 -0400

    making updates for review

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,21 +1,5 @@
 module ReportsHelper
 
-  #calculating the proportional font size for each number or errors,
-  #given the high and low numbers of errors in the set
-  def problem_font_size(error_list, item_errors)
-    max_font = 48
-    min_font = 12
-    error_range = (error_list[0][1] - error_list[-1][1]).to_f
-    font_range = (max_font - min_font).to_f
-    if error_range == 0
-      return min_font.to_s
-    end 
-
-    item_difference = item_errors - error_list[-1][1]
-
-    return ((font_range/error_range) * item_difference + min_font).to_s     
-  end
-
   def list_past_months
     @review_month_counts = []
     #this should be optimized to bucket all the reviews in one run through
