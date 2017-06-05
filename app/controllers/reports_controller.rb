0c14e7e8184fc4184c3a4616959cfba210b74d1b
Andrew Baker <abaker@element84.com>
2017-06-05 10:43:02 -0400

    moving library from global to report only

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -50,7 +50,7 @@ class ReportsController < ApplicationController
     @quality_done_records = metric_set.quality_done_records
 
     respond_to do |format|
-      format.html
+      format.html { render :layout => 'reports' }
       format.csv { send_data(render_to_string, filename: "cmr_dashboard_metrics.csv") }
     end
   end
@@ -107,7 +107,7 @@ class ReportsController < ApplicationController
     end
 
     respond_to do |format|
-      format.html { render :template => "reports/home" }
+      format.html { render :template => "reports/home", :layout => "reports" }
       format.csv { send_data(render_to_string, filename: "#{@daac}_metrics.csv") }
     end
   end
@@ -176,7 +176,7 @@ class ReportsController < ApplicationController
     @quality_done_records = metric_set.quality_done_records
 
     respond_to do |format|
-      format.html { render :template => "reports/home" }
+      format.html { render :template => "reports/home", :layout => "reports" }
       format.csv { send_data(render_to_string, filename: "cmr_selection_metrics.csv") }
     end                          
   end
