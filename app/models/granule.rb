309b389023d62e653b06b25d5be9f9653f980ce7
Nate Pauzenga <npauzenga@gmail.com>
2017-08-09 17:00:06 -0400

    add nil checks to replace_all_granules rake
    My local db was throwing errors on a few collections. The checks added
    here cover the problems I was having, likely due to my local being in a
    weird state.
    
    We’ll want to find a decent way to test this with a mirror of the
    production database as I can’t point to exactly why some of my
    collection were throwing errors. It was likely just old/bad data but I
    can’t be sure.

--- app/models/granule.rb
+++ app/models/granule.rb
@@ -7,32 +7,34 @@ class Granule < ActiveRecord::Base
     granules_to_save = Cmr.random_granules_from_collection(concept_id, granules_count)
     #replacing the data with new granule & record & ingest objects
     granules_components = []
-    granules_to_save.each do |granule_data|
-      #creating the granule and related record 
-      granule_object = Granule.new(concept_id: granule_data["concept_id"], collection: collection_object)
-      new_granule_record = Record.new(recordable: granule_object, revision_id: granule_data["revision_id"])
-      new_granule_record.save
 
-      granule_record_data_list = []
+    if (granules_to_save.is_a? Array)
+      granules_to_save.each do |granule_data|
+        #creating the granule and related record 
+        granule_object = Granule.new(concept_id: granule_data["concept_id"], collection: collection_object)
+        new_granule_record = Record.new(recordable: granule_object, revision_id: granule_data["revision_id"])
+        new_granule_record.save
 
-      flattened_granule_data = Cmr.get_granule(granule_data['concept_id'])
+        granule_record_data_list = []
 
-      #creating all the recordData values for the granule
-      flattened_granule_data.each_with_index do |(key, value), i|
-        granule_record_data = RecordData.new(record: new_granule_record)
-        granule_record_data.last_updated = DateTime.now
-        granule_record_data.column_name = key
-        granule_record_data.value = value
-        granule_record_data.order_count = i
-        granule_record_data.daac = granule_data["concept_id"].partition('-').last
-        granule_record_data_list.push(granule_record_data)
-      end
+        flattened_granule_data = Cmr.get_granule(granule_data['concept_id'])
 
-      granule_ingest = Ingest.new(record: new_granule_record, user: current_user, date_ingested: DateTime.now)
-      #pushing the list of granule parts into the granule_components list for a return value
-      granules_components.push([ granule_object, new_granule_record, granule_record_data_list, granule_ingest ])
-    end 
+        #creating all the recordData values for the granule
+        flattened_granule_data.each_with_index do |(key, value), i|
+          granule_record_data = RecordData.new(record: new_granule_record)
+          granule_record_data.last_updated = DateTime.now
+          granule_record_data.column_name = key
+          granule_record_data.value = value
+          granule_record_data.order_count = i
+          granule_record_data.daac = granule_data["concept_id"].partition('-').last
+          granule_record_data_list.push(granule_record_data)
+        end
 
+        granule_ingest = Ingest.new(record: new_granule_record, user: current_user, date_ingested: DateTime.now)
+        #pushing the list of granule parts into the granule_components list for a return value
+        granules_components.push([ granule_object, new_granule_record, granule_record_data_list, granule_ingest ])
+      end 
+    end
     granules_components
   end
 end
