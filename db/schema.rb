dd72857d08a067ecb93cb43ea24d7eb99f645b39
Paul Pilone <paul@element84.com>
2017-11-10 10:43:47 -0500

    [WIP]: Adds migration to user. Adds initial form inputs.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612013938) do
+ActiveRecord::Schema.define(version: 20171110151240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,6 +108,8 @@ ActiveRecord::Schema.define(version: 20170612013938) do
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
     t.boolean  "curator",                default: false
+    t.boolean  "daac_curator",           default: false
+    t.string   "daac"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
