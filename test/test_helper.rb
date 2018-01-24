f12ceb244535b61a5c91c71f4eda428aceef0b2b
Brian Tennity <brian@element84.com>
2018-01-24 16:17:58 -0500

    Add Mocha to easily stub objects for testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ SimpleCov.start 'rails'
 
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'mocha/mini_test'
 require 'minitest/mock'
 require 'webmock/minitest'
 
