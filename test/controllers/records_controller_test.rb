375e4a16e787fd215ba4a48b978d3e98604c4ca1
Brian Tennity <brian@element84.com>
2018-01-24 15:41:40 -0500

    Fix spacing in RecordsControllerTest

--- test/controllers/records_controller_test.rb
+++ test/controllers/records_controller_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
 class RecordsControllerTest < ActionController::TestCase
-  describe "POST #create" do  
+  describe "POST #create" do
     it "updates record data from review params" do
       @tester = User.find_by_email("abaker@element84.com")
       sign_in(@tester)
       #sample update params
-      #posting with a record id param of 1       
+      #posting with a record id param of 1
       post :update, {"redirect_index"=>"0", "recommendation"=>{"ShortName"=>"", "VersionId"=>"not ok", "LongName"=>"ok"}, "opinion"=>{"VersionId"=>"on"}, "color_code"=>{"ShortName"=>"", "VersionId"=>"red", "LongName"=>""}, "discussion"=>{"ShortName"=>"", "VersionId"=>"", "LongName"=>"new comment"}, "utf8"=>"✓", "_method"=>"put", "authenticity_token"=>"rKpXaDgn6x9I1T3gNZ4cpEYZZsWORy4gnLAPTNsZIaRWxFWRM6trFGVDe8BkL8kklhC00LikvGn1Uau2KAA4fg==", "section_index"=>"0", "commit"=>"Save Review", "controller"=>"records", "action"=>"update", "id"=>"1"}
 
       record = Record.find_by id: 1
@@ -17,15 +17,16 @@ class RecordsControllerTest < ActionController::TestCase
       assert_equal(record.get_recommendations["ShortName"], "")
       assert_equal(record.get_recommendations["LongName"], "ok")
 
-      assert_equal(record.get_opinions["VersionId"], true)      
+      assert_equal(record.get_opinions["VersionId"], true)
 
       assert_equal(record.get_colors["VersionId"], "red")
       assert_equal(record.get_colors["ShortName"], "")
       assert_equal(record.get_colors["LongName"], "")
 
+      assert_equal(record.get_opinions["VersionId"], true)
 
       #"discussion"=>{"ShortName"=>"", "VersionId"=>"", "InsertTime"=>"", "LastUpdate"=>"", "LongName"=>"new comment", "DataSetId"=>"", "Description"=>"", "CollectionDataType"=>"", "Orderable"=>"", "Visible"=>"", "ProcessingLevelId"=>"", "ArchiveCenter"=>"", "CitationForExternalPublication"=>"", "Price"=>"", "SpatialKeywords/Keyword"=>"", "TemporalKeywords/Keyword"=>"", "AssociatedDIFs/DIF/EntryId"=>"", "MetadataStandardName"=>"", "MetadataStandardVersion"=>"", "DatasetId"=>"", "DataFormat"=>""}
-      assert_equal(record.discussions.where(column_name: "LongName").sort_by(&:date).first.comment, "new comment")      
+      assert_equal(record.discussions.where(column_name: "LongName").sort_by(&:date).first.comment, "new comment")
     end
 
     it "removes values when unselected" do
@@ -46,14 +47,14 @@ class RecordsControllerTest < ActionController::TestCase
       assert_equal(record.get_recommendations["ShortName"], "")
       assert_equal(record.get_recommendations["LongName"], "")
 
-      assert_equal(record.get_opinions["VersionId"], false)      
+      assert_equal(record.get_opinions["VersionId"], false)
 
       assert_equal(record.get_colors["VersionId"], "")
       assert_equal(record.get_colors["ShortName"], "")
       assert_equal(record.get_colors["LongName"], "")
 
       #discussion should not be changed, checking to ensure its still there.
-      assert_equal(record.discussions.where(column_name: "LongName").sort_by(&:date).first.comment, "new comment")      
+      assert_equal(record.discussions.where(column_name: "LongName").sort_by(&:date).first.comment, "new comment")
     end
   end
 
