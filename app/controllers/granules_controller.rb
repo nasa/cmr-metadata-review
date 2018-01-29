7945c4417f26774e1d100dff13c06dcf4464f2f5
Brian Tennity <brian@element84.com>
2018-01-29 16:05:20 -0500

    Update CanCan authorization for GranulesController#refresh

--- app/controllers/granules_controller.rb
+++ app/controllers/granules_controller.rb
@@ -11,15 +11,15 @@ class GranulesController < ApplicationController
     record     = granule.records.find(params[:record_id])
     collection = granule.collection
 
-    if !can?(:refresh_granule, granule)
-      flash[:alert] = "You do not have permission to perform this action"
-    elsif !record.open?
-      flash[:alert] = "This granule is in review, and can no longer be changed to a different granule"
-    else
+    authorize! :refresh_granule, granule
+
+    if record.open?
       granule.delete_self
       collection.add_granule(current_user)
       
       flash[:notice] = "A new granule has been selected for this collection"
+    else
+      flash[:alert] = "This granule is in review, and can no longer be changed to a different granule"
     end
 
     redirect_to collection_path(id: 1, concept_id: collection.concept_id)
