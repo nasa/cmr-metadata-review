d9458f64431a1bc931be8d0a1bdf1ea210d92533
Brian Tennity <brian@element84.com>
2018-01-29 13:56:46 -0500

    Add button to choose a new granule to review for a collection

--- app/controllers/granules_controller.rb
+++ app/controllers/granules_controller.rb
@@ -5,4 +5,15 @@ class GranulesController < ApplicationController
 
   def show
   end
+
+  def refresh
+    granule    = Granule.find(params[:id])
+    collection = granule.collection
+
+    granule.delete_self
+    collection.add_granule(current_user)
+    
+    flash[:notice] = "Granule has been refreshed"
+    redirect_to collection_path(id: 1, concept_id: collection.concept_id)
+  end
 end
