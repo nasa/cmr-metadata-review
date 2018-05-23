0391b515258a5d78ccbd20399ca5637a3e012dc8
Brian Tennity <brian@element84.com>
2018-05-23 14:12:45 -0400

    Update MetricSet#element_non_green_count to query the DB instead of counting through each RecordData

--- app/models/metric_set.rb
+++ app/models/metric_set.rb
@@ -37,36 +37,10 @@ class MetricSet
   # ====Returns
   # Array of [column_name, non_green_count] sets.  Double depth array
   # ==== Method
-  # This method iterates through all the record data objects of the metric set's records
-  # It then created a new hash where each column name is a key and a hash of non green flag counts is the value.
-  # With this hash you can call any column name and it returns a hash of the counts of each non green flag
-  #
-  # The method then iterates through the hash creating set objects of [column_name, all non green counts summed]
-  # The purpose of this list is to tie the total non green count for all record data to each column name.
-  #
-  # Finally the method sorts all of the [column_name, counts] sets to find the column_names with the most non green flags.
-  # This sorted list is returned so the user can grab the top n many column_names sorted by non green flag count
+  # This method queries the DB for the ten elements with the most errors
 
   def element_non_green_count
-    element_hash = {}
-    #creating a hash with keys of each column_name
-    @record_data_set.each do |record_data|
-      if !element_hash.key? record_data.column_name
-        element_hash[record_data.column_name] = {"green" => 0, "red" => 0, "blue" => 0, "yellow" => 0}
-      end
-      if element_hash[record_data.column_name].key? record_data.color
-        element_hash[record_data.column_name][record_data.color] = element_hash[record_data.column_name][record_data.color] + 1
-      end
-    end
-
-    #tuning hash into list of [column_name, non_green_count, green_count] sets
-    column_counts = []
-    element_hash.map do |column_name, counts_hash|
-      column_counts.push([column_name, (counts_hash.values.sum - counts_hash["green"]), counts_hash["green"]])
-    end
-
-    #sorting in reverse order so highest counts are first
-    column_counts.sort! { |x,y| y[1] <=> x[1] }
+    @record_data_set.where(color: ["red", "yellow", "blue"]).group(:column_name).order('count_all DESC').limit(10).count.to_a
   end
 
   private
