621107d546a364138489137e7fd87d1ed1d33d42
Christopher Gokey <christopher.d.gokey@nasa.gov>
2018-07-05 14:59:23 -0400

    MMT-1509 Ingest different granule option is now always available even if the record is closed.

--- app/controllers/granules_controller.rb
+++ app/controllers/granules_controller.rb
@@ -7,20 +7,15 @@ class GranulesController < ApplicationController
   end
 
   def replace
-    granule    = Granule.find(params[:id])
-    record     = granule.records.find(params[:record_id])
+    granule = Granule.find(params[:id])
+    record = granule.records.find(params[:record_id])
     collection = granule.collection
 
     authorize! :replace_granule, granule
 
-    if record.open?
-      granule.destroy
-      collection.add_granule(current_user)
-
-      flash[:notice] = "A new granule has been selected for this collection"
-    else
-      flash[:alert] = "This granule is in review, and can no longer be changed to a different granule"
-    end
+    granule.destroy
+    collection.add_granule(current_user)
+    flash[:notice] = "A new granule has been selected for this collection"
 
     redirect_to collection_path(id: 1, record_id: collection.records.first.id)
   end
