897656ae137417a23a63272e856b683ebb3b6da5
Christopher Gokey <christopher.d.gokey@nasa.gov>
2018-07-09 10:41:15 -0400

    MMT-1509 - Unable to delete/ import a different granule for in progress records.

--- app/controllers/granules_controller.rb
+++ app/controllers/granules_controller.rb
@@ -7,20 +7,28 @@ class GranulesController < ApplicationController
   end
 
   def replace
-    granule    = Granule.find(params[:id])
-    record     = granule.records.find(params[:record_id])
+    granule = Granule.find(params[:id])
+    record = granule.records.find(params[:record_id])
     collection = granule.collection
 
     authorize! :replace_granule, granule
 
-    if record.open?
-      granule.destroy
-      collection.add_granule(current_user)
+    # Removing temporarily until we come up with a process flow that allows them to reopen the record after it has
+    # been closed.   A new ticket should be created allowing them to reopen a record.
 
-      flash[:notice] = "A new granule has been selected for this collection"
-    else
-      flash[:alert] = "This granule is in review, and can no longer be changed to a different granule"
-    end
+    # if record.open?
+    #   granule.destroy
+    #   collection.add_granule(current_user)
+    #
+    #   flash[:notice] = "A new granule has been selected for this collection"
+    # else
+    #   flash[:alert] = "This granule is in review, and can no longer be changed to a different granule"
+    # end
+    #
+
+    granule.destroy
+    collection.add_granule(current_user)
+    flash[:notice] = "A new granule has been selected for this collection"
 
     redirect_to collection_path(id: 1, record_id: collection.records.first.id)
   end
