10e263e5a58f19e768259007ccd0e60c2ba35efe
Hoan <hv.tranho@gmail.com>
2018-07-19 14:20:09 -0400

    MMT-1510 user test added

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -13,4 +13,23 @@ class UserTest < ActiveSupport::TestCase
 
 	end
 
+	describe "ARC curator role" do
+
+    it "can see screen 'Awaiting Release to DAAC' but cannot release or delete a record" do
+      arc_user = User.new
+      arc_user.role = 'arc_curator'
+
+      assert arc_user.role.eql?('arc_curator')
+
+      ability = Ability.new(arc_user)
+      #User should be able to access 'Awaiting Release to DAAC'
+      assert ability.can?(:review_state, Record::STATE_READY_FOR_DAAC_REVIEW)
+      #User should not be able to release a record to DAAC
+      assert_not ability.can?(:release_to_daac, Record)
+      #User should not be able to delete a record
+      assert_not arc_user.admin
+
+    end
+  end
+
 end
