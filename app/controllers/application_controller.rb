71ff499175dfaf9a950053691b2c4a6a57bea3eb
Christopher Gokey <christopher.d.gokey@nasa.gov>
2018-07-30 15:10:08 -0400

    MMT-1473 - removed rescue_from routing error which no longer works in later version of rails.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  # http_basic_authenticate_with name: "cmruser", password: "dashpass"
 
-  rescue_from ActionController::RoutingError, :with => :render_404
   private
   def render_404(exception = nil)
     if exception
