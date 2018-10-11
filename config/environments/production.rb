c99ec6d5cb14d0972d10e731ed9bf365efef484d
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2018-10-11 13:35:11 -0400

    Removed configuration value to not perform mail deliveries and just to be sure commented out the code in the mailer class that actually performs the mailing.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
