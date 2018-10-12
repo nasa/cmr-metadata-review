7ee7c54360c2dc608867ca89fbed5877028d09b1
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2018-10-12 14:56:01 -0400

    CMRARC-406 commented out entire block

--- test/models/record_notifier_test.rb
+++ test/models/record_notifier_test.rb
@@ -2,28 +2,28 @@ require 'test_helper'
 
 class RecordNotifierTest < ActiveSupport::TestCase
 
-  describe "notify_released" do
-
-    # it 'should email daac curators for released records' do
-    #   records = Record.find([12, 14]).to_a
-    #
-    #   assert_difference 'ActionMailer::Base.deliveries.size', +2 do
-    #     RecordNotifier.notify_released(records)
-    #   end
-    # end
-
-  end
-
-  describe "notify_closed" do
-
-    # it 'should email daac curators for closed records' do
-    #   records = Record.find([15]).to_a
-    #
-    #   assert_difference 'ActionMailer::Base.deliveries.size', +1 do
-    #     RecordNotifier.notify_closed(records)
-    #   end
-    # end
-
-  end
+  # describe "notify_released" do
+  #
+  #   it 'should email daac curators for released records' do
+  #     records = Record.find([12, 14]).to_a
+  #
+  #     assert_difference 'ActionMailer::Base.deliveries.size', +2 do
+  #       RecordNotifier.notify_released(records)
+  #     end
+  #   end
+  #
+  # end
+  #
+  # describe "notify_closed" do
+  #
+  #   it 'should email daac curators for closed records' do
+  #     records = Record.find([15]).to_a
+  #
+  #     assert_difference 'ActionMailer::Base.deliveries.size', +1 do
+  #       RecordNotifier.notify_closed(records)
+  #     end
+  #   end
+  #
+  # end
 
 end
