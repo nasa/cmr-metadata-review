72aca0a262d6eac533f2909182f955db2045c513
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2018-10-12 14:26:13 -0400

    Revert "CMRARC-406 had to comment out mailer tests too"
    This reverts commit 07d8a5d59220135a4b072a9fb22f609486568ed8.

--- test/models/record_notifier_test.rb
+++ test/models/record_notifier_test.rb
@@ -5,11 +5,11 @@ class RecordNotifierTest < ActiveSupport::TestCase
   describe "notify_released" do
 
     it 'should email daac curators for released records' do
-      # records = Record.find([12, 14]).to_a
-      #
-      # assert_difference 'ActionMailer::Base.deliveries.size', +2 do
-      #   RecordNotifier.notify_released(records)
-      # end
+      records = Record.find([12, 14]).to_a
+
+      assert_difference 'ActionMailer::Base.deliveries.size', +2 do
+        RecordNotifier.notify_released(records)
+      end
     end
 
   end
@@ -17,11 +17,11 @@ class RecordNotifierTest < ActiveSupport::TestCase
   describe "notify_closed" do
 
     it 'should email daac curators for closed records' do
-      # records = Record.find([15]).to_a
-      #
-      # assert_difference 'ActionMailer::Base.deliveries.size', +1 do
-      #   RecordNotifier.notify_closed(records)
-      # end
+      records = Record.find([15]).to_a
+
+      assert_difference 'ActionMailer::Base.deliveries.size', +1 do
+        RecordNotifier.notify_closed(records)
+      end
     end
 
   end
