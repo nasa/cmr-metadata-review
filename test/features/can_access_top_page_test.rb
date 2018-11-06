2e774a003b6e2ea398a9972758073adad724bb74
Thomas Cherry <thomas.cherry@gmail.com>
2018-11-06 14:42:25 -0500

    CMRARC-417 - updating the gem Rack to the latest it its range (#54)
    * CMRARC-355 Removed views/users directory as not necessary.
    
    * CMRARC-355 added tests for logging in and callbacks
    
    * CMRARC-355 Updated based on feedback.
    
    * CMRARC-355 added features and few other minor changes
    
    * CMRARC-355 included where to find macros
    
    * CMRARC-355 now uses develop branch, will eventually be changed to use a tag.
    
    * CMRARC-355 Needed to remove password from seeds.rb
    
    * CMRARC-355 not sure why but since omniauth-urs gem included dotenv,
    the .env.test file isn't overriding .env, so have to do the above
    trick to recognize it.
    
    * Changed <%- to just <%
    
    * updated mock response
    
    * removed comment
    
    * CMRARC-355 added helper method for camelizing str.
    
    * CMRARC-355 refactored so retrieving display name was in the helper method.
    
    * CMRARC-354 pass in env variables to earth data login urls through client options.
    
    * CMRARC-407 Functionality added to derive the role (arc curator,
    daac curator, or admin) from ACLs.
    
    * CMRARC-407 Updated client id with env variable instead.
    
    * Delete urs_app.rb
    
    This file is not needed.
    
    * CMRARC-407 modified code based on feedback.   removed not used code,
    added some comments, etc.
    
    * CMRARC-417 - updating the gem Rack to the latest it its range

---
+++ test/features/can_access_top_page_test.rb
@@ -0,0 +1,78 @@
+require "test_helper"
+Dir[Rails.root.join("test/**/*.rb")].each {|f| require f}
+
+class CanAccessTopPageTest < Capybara::Rails::TestCase
+  include OmniauthMacros
+
+  describe "POST #urs" do
+    before do
+      OmniAuth.config.test_mode = true
+      mock_auth_hash
+      Rails.application.env_config["devise.mapping"] = Devise.mappings[:user] # If using Devise
+      Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:urs]
+
+      stub_request(:get, "https://cmr.sit.earthdata.nasa.gov/access-control/acls?page_num=1&page_size=2000&permitted_user=12345").
+        with(
+          headers: {
+            'Accept'=>'*/*',
+            'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'Echo-Token'=>'12345:',
+            'User-Agent'=>'Faraday v0.15.3'
+          }).
+        to_return(status: 200, body: '{"hits":1,"took":661,"items":[{"revision_id":16,"concept_id":"ACL1200213993-CMR","identity_type":"Catalog Item","name":"Admin Full Access","location":"https://cmr.sit.earthdata.nasa.gov:443/access-control/acls/ACL1200213993-CMR", {"revision_id":1,"concept_id":"ACL1200301611-CMR","identity_type":"System","name":"System - DASHBOARD_ADMIN","location":"https://cmr.sit.earthdata.nasa.gov:443/access-control/acls/ACL1200301611-CMR"}}]}', headers: {})
+
+    end
+
+    describe "access top page" do
+      it "can sign in user with oauth account with admin privileges" do
+        mock_auth_hash
+
+        stub_request(:get, "https://cmr.sit.earthdata.nasa.gov/access-control/acls?page_num=1&page_size=2000&permitted_user=12345").
+          with(
+            headers: {
+              'Accept'=>'*/*',
+              'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+              'Echo-Token'=>'12345:',
+              'User-Agent'=>'Faraday v0.15.3'
+            }).
+          to_return(status: 200, body: '{"hits":1,"took":661,"items":[{"revision_id":16,"concept_id":"ACL1200213993-CMR","identity_type":"Catalog Item","name":"Admin Full Access","location":"https://cmr.sit.earthdata.nasa.gov:443/access-control/acls/ACL1200213993-CMR"},{"revision_id":1,"concept_id":"ACL1200301611-CMR","identity_type":"System","name":"System - DASHBOARD_ADMIN","location":"https://cmr.sit.earthdata.nasa.gov:443/access-control/acls/ACL1200301611-CMR"}]}', headers: {})
+
+        visit '/'
+        page.must_have_content("Login with Earthdata Login")
+        click_link "Login"
+        page.must_have_content("Logout")
+        page.must_have_content("Account Options")
+      end
+
+      it "can sign in user with oauth account with arc curator privileges" do
+        mock_auth_hash
+
+        stub_request(:get, "https://cmr.sit.earthdata.nasa.gov/access-control/acls?page_num=1&page_size=2000&permitted_user=12345").
+          with(
+            headers: {
+              'Accept'=>'*/*',
+              'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+              'Echo-Token'=>'12345:',
+              'User-Agent'=>'Faraday v0.15.3'
+            }).
+          to_return(status: 200, body: '{"hits":1,"took":661,"items":[{"revision_id":16,"concept_id":"ACL1200213993-CMR","identity_type":"Catalog Item","name":"Admin Full Access","location":"https://cmr.sit.earthdata.nasa.gov:443/access-control/acls/ACL1200213993-CMR"},{"revision_id":1,"concept_id":"ACL1200301610-CMR","identity_type":"System","name":"System - DASHBOARD_ARC_CURATOR","location":"https://cmr.sit.earthdata.nasa.gov:443/access-control/acls/ACL1200301610-CMR"}]}', headers: {})
+
+        visit '/'
+        page.must_have_content("Login with Earthdata Login")
+        click_link "Login"
+        page.must_have_content("Logout")
+        page.wont_have_content("Account Options")
+      end
+
+
+      it "can handle authentication error" do
+        OmniAuth.config.mock_auth[:urs] = :invalid_credentials
+        visit '/'
+        page.must_have_content("Login with Earthdata Login")
+        click_link "Login"
+        page.must_have_content('Could not authenticate you from URS because "Invalid credentials".')
+      end
+    end
+
+  end
+end
