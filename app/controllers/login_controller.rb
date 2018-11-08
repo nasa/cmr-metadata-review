4fe9f7f381ecd6a74d5a2e4eeead31c955bcc1af
cgokey <cgokey@comcast.net>
2018-11-08 11:14:56 -0500

    Feature/earth data login (#56)
    * CMRARC-355 Removed views/users directory as not necessary.
    
    * CMRARC-355 added tests for logging in and callbacks
    
    * CMRARC-355 Updated based on feedback.
    
    * CMRARC-355 added features and few other minor changes
    
    * CMRARC-355 included where to find macros
    
    * CMRARC-355 now uses develop branch, will eventually be changed to use a tag.
    
    * CMRARC-355 Needed to remove password from seeds.rb
    
    * CMRARC-355 not sure why but since omniauth-urs gem included dotenv,
    the .env.test file isn't overriding .env, so have to do the above
    trick to recognize it.
    
    * Changed <%- to just <%
    
    * updated mock response
    
    * removed comment
    
    * CMRARC-355 added helper method for camelizing str.
    
    * CMRARC-355 refactored so retrieving display name was in the helper method.
    
    * CMRARC-354 pass in env variables to earth data login urls through client options.
    
    * CMRARC-407 Functionality added to derive the role (arc curator,
    daac curator, or admin) from ACLs.
    
    * CMRARC-407 Updated client id with env variable instead.
    
    * Delete urs_app.rb
    
    This file is not needed.
    
    * CMRARC-407 modified code based on feedback.   removed not used code,
    added some comments, etc.
    
    * Hotfix/cmrarc 417 (#55)
    
    * CMRARC-415 - updating an outdated gem by request
    
    * CMRARC-415 - adding files used to help conduct tests (database start) and have text completion in vim
    
    * CMRARC-415 - adding documentation to the start script per feedback
    
    * CMRARC-417 - updating the gem Rack to the latest it its range
    
    * CMRARC-417 - updating the gem Rack to the latest it its range (#54)
    
    * CMRARC-355 Removed views/users directory as not necessary.
    
    * CMRARC-355 added tests for logging in and callbacks
    
    * CMRARC-355 Updated based on feedback.
    
    * CMRARC-355 added features and few other minor changes
    
    * CMRARC-355 included where to find macros
    
    * CMRARC-355 now uses develop branch, will eventually be changed to use a tag.
    
    * CMRARC-355 Needed to remove password from seeds.rb
    
    * CMRARC-355 not sure why but since omniauth-urs gem included dotenv,
    the .env.test file isn't overriding .env, so have to do the above
    trick to recognize it.
    
    * Changed <%- to just <%
    
    * updated mock response
    
    * removed comment
    
    * CMRARC-355 added helper method for camelizing str.
    
    * CMRARC-355 refactored so retrieving display name was in the helper method.
    
    * CMRARC-354 pass in env variables to earth data login urls through client options.
    
    * CMRARC-407 Functionality added to derive the role (arc curator,
    daac curator, or admin) from ACLs.
    
    * CMRARC-407 Updated client id with env variable instead.
    
    * Delete urs_app.rb
    
    This file is not needed.
    
    * CMRARC-407 modified code based on feedback.   removed not used code,
    added some comments, etc.
    
    * CMRARC-417 - updating the gem Rack to the latest it its range
    
    * CMRARC-407 - gem lock file was not correct
    
    * CMRARC-407 - gem lock file was not correct, dropped some gems in a bad merge
    
    * CMRARC-407 changed implementation based on feedback.
    
    * CMRARC-407 added stub_requests for login controller test.
    
    * CMRARC-417 and CMRARC-415 - bundle audit updates
    
    * CMRARC-407 fixed unit tests to take into account different cmr environments.
    
    * CMRARC-407 Further changes based on feedback from pull request.
    
    * CMRARC-407 removed ()'s in if statement.
    
    * CMRARC-407 changed {} to do end

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -3,12 +3,20 @@ class LoginController < Devise::OmniauthCallbacksController
   def urs
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
-    if @user.persisted?
+    # before we sign user in, make sure two things:
+    # 1) the user is in database, they should be as the result of calling User.from_omniauth
+    # 2) A role has been assigned to the user.   In order to be given a role, they must have an ACL in CMR.  If they
+    # don't the role will be nil.
+    if @user.persisted? && !@user.role.nil?
       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: "Earth Data Login") if is_navigational_format?
     else
       redirect_to root_path
-      flash.notice = "Could not authenticate from Earth Data Login"
+      if (@user.role.nil?)
+        flash.notice = "User is not provisioned with the proper ACLs.   Please contact the Operations team."
+      else
+        flash.notice = "Could not authenticate from Earth Data Login"
+      end
     end
   end
 end
