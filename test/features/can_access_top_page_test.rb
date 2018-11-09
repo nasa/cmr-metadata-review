c03ec4b9ee1c653a0f05904216ff970521641d1d
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2018-11-09 11:49:37 -0500

    CMRARC-418 * changed wording regarding if a user is not provisioned. * pulls provider_id from acl instead of parsing * improved testings, looking for each section per the type of role user is provisioned with.

--- test/features/can_access_top_page_test.rb
+++ test/features/can_access_top_page_test.rb
@@ -43,6 +43,13 @@ class CanAccessTopPageTest < Capybara::Rails::TestCase
         page.must_have_content("Logout")
         page.must_have_content("Account Options")
         page.must_have_content("Unreviewed Records:")
+        page.must_have_content("In ARC Review Records:")
+        page.must_have_content("Awaiting Release to DAAC:")
+        page.must_have_content("In DAAC Review:")
+        page.must_have_content("Requires Curator Feedback Records:")
+
+
+
       end
 
       it "can sign in user with oauth account with arc curator privileges" do
@@ -64,11 +71,25 @@ class CanAccessTopPageTest < Capybara::Rails::TestCase
         page.must_have_content("Logout")
         page.wont_have_content("Account Options")
         page.must_have_content("Unreviewed Records:")
+        page.must_have_content("In ARC Review Records:")
+        page.must_have_content("Awaiting Release to DAAC:")
+        page.wont_have_content("In DAAC Review:")
+        page.must_have_content("Requires Curator Feedback Records:")
 
       end
 
       it "can sign in user with oauth account with daac curator privileges" do
         mock_auth_hash
+
+        stub_request(:get, "https://cmr.sit.earthdata.nasa.gov/access-control/acls/ACL1200303063-CMR").
+          with(
+            headers: {
+              'Accept'=>'*/*',
+              'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+              'Echo-Token'=>'12345:',
+              'User-Agent'=>'Faraday v0.15.3'
+            }).
+          to_return(status: 200, body: '{"group_permissions":[{"group_id":"AG1200303062-LARC","permissions":["create"]},{"group_id":"AG1200301542-CMR","permissions":["create"]},{"group_id":"AG1200303012-CMR","permissions":["create"]}],"provider_identity":{"target":"DASHBOARD_DAAC_CURATOR","provider_id":"LARC"}}', headers: {})
 
         stub_request(:get, "#{Cmr.get_cmr_base_url}/access-control/acls?page_num=1&page_size=2000&permitted_user=12345").
           with(
@@ -83,13 +104,11 @@ class CanAccessTopPageTest < Capybara::Rails::TestCase
         page.must_have_content("Login with Earthdata Login")
         click_link "Login"
         page.must_have_content("Logout")
+        page.must_have_content("In DAAC Review:")
+        page.must_have_content("Requires Curator Feedback Records:")
         page.wont_have_content("Unreviewed Records:")
       end
-
-
-
-
-
+      
       it "can handle authentication error" do
         OmniAuth.config.mock_auth[:urs] = :invalid_credentials
         visit '/'
