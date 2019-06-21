a85987ab62a243cb5d3bf600c8a695d58018883b
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2019-06-20 22:21:03 -0400

    CMRARC-431 - uses headless chrome and added a feature test.

---
+++ test/helpers/user_helpers.rb
@@ -0,0 +1,14 @@
+module Helpers
+  module UserHelpers
+    # by passes devise methods needed to login
+    # you can pass either id or uid or role and will return the user.
+    def mock_login(id: nil, uid: nil, role: nil)
+      user = User.find_by role: role unless role == nil
+      user = User.find_by uid: uid unless uid == nil
+      user = User.find_by id: id unless id == nil
+      ApplicationController.any_instance.stubs(:authenticate_user!).returns(true)
+      ApplicationController.any_instance.stubs(:user_signed_in).returns(true)
+      ApplicationController.any_instance.stubs(:current_user).returns(user)
+    end
+  end
+end
