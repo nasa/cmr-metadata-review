c2a9e81586eef50a4d3377908a15df80364c8b99
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2019-06-24 13:10:10 -0400

    CMRARC-431 - configured driver same as mmt, thinking issue might be because I didn't have nosandbox arg.   Also fixed test to work with link instead of button.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 ENV['RAILS_ENV'] ||= 'test'
 require 'simplecov'
 SimpleCov.start 'rails'
 
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
 require 'mocha/minitest'
 require 'minitest/mock'
@@ -12,23 +14,43 @@ require 'minitest/rails/capybara'
 
 Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new, Minitest::Reporters::JUnitReporter.new]
 
+# Specs flagged with `js: true` will use Capybara's JS driver.
 Capybara.register_driver :headless_chrome do |app|
+  # set timeout to 60s http://www.testrisk.com/2016/05/change-default-timeout-and-wait-time-of.html
+  # need to use read_timeout and open_timeout https://github.com/SeleniumHQ/selenium/blob/master/rb/lib/selenium/webdriver/remote/http/default.rb
+  client = Selenium::WebDriver::Remote::Http::Default.new
+  client.read_timeout = 60
+  client.open_timeout = 60
+
+  # http://technopragmatica.blogspot.com/2017/10/switching-to-headless-chrome-for-rails_31.html
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w[headless disable-gpu --window-size=1500,1500] }
+    # This makes javascript console logs available, but doesn't cause them to appear in real time
+    # to display javascript logs in the rspec output, add `puts page.driver.browser.manage.logs.get(:browser)`
+    # in the desired test location
+    loggingPrefs: { browser: 'ALL', client: 'ALL', driver: 'ALL', server: 'ALL' }
   )
 
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    desired_capabilities: capabilities
+  # disable-gpu option is temporarily necessary, possibly only for Windows
+  # https://developers.google.com/web/updates/2017/04/headless-chrome#cli
+  # no-sandbox was necessary for another application's Docker container for CI/CD
+  # https://about.gitlab.com/2017/12/19/moving-to-headless-chrome/
+  # https://developers.google.com/web/updates/2017/04/headless-chrome#faq
+  options = Selenium::WebDriver::Chrome::Options.new(
+    args: %w[headless disable-gpu no-sandbox --window-size=1500,1500]
   )
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, http_client: client, desired_capabilities: capabilities, options: options)
 end
 
 # setting headless_chrome as default driver, can be changed to run not headless
 Capybara.default_driver = :headless_chrome
 Capybara.javascript_driver = :headless_chrome
 # Not sure this is the best thing to do, but we don't call localhost for anything needed in our tests.
-WebMock.disable_net_connect!(allow_localhost: true)
+
+WebMock.disable_net_connect!(
+  allow_localhost: true,
+  allow: 'chromedriver.storage.googleapis.com'
+)
 # WebMock.allow_net_connect!
 WebMock.after_request(real_requests_only: true) do |request_signature, response|
   unless request_signature.uri.to_s.include?('127.0.0.1')
@@ -57,4 +79,3 @@ end
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
-
