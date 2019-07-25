f72f444f152fd0f4c7cc2a319a02ac66b1ee27f7
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2019-07-25 16:51:52 -0400

    CMRARC-462 moved js to assets, including necessary libs in gemfile, handled cancan bit better, handled error handing if record saved a bit better.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui
 //= require turbolinks
 //= require loading_modal
 //= require d3/d3.min
---
+++ app/assets/javascripts/collections-associations.js
@@ -0,0 +1,44 @@
+$(document).on('turbolinks:load', function() {
+  var revisionIdElement;
+
+  $(".associated_granule_value").change(function (event) {
+    var target = event.target;
+    var revision_id = target.getAttribute("revision_id");
+    var form_id = "#form-" + revision_id;
+    $(form_id).submit()
+  });
+
+  $(".granule_revision")
+    .mouseover(function (event) {
+      // event.target.addClass("mouse_over_draggable");
+    })
+    .mouseout(function (event) {
+      // event.target.removeClass("mouse_over_draggable");
+    });
+
+  $(".granule_revision").draggable({
+    start: function (event, ui) {
+      revisionIdElement = event.target;
+    },
+    stop: function (event, ui) {
+      revisionIdElement.style.top = 0;
+      revisionIdElement.style.left = 0;
+    }
+  });
+
+// $( "#collection_revision" ).droppable( "option", "accept", "#granule_revision" );
+  $(".collection_revision").droppable({
+    over: function (event, ui) {
+      // event.target.addClass("revision_id_over_collection");
+    },
+    out: function (event, ui) {
+      // event.target.removeClass("revision_id_over_collection");
+    },
+    drop: function (event, ui) {
+      var granuleId = revisionIdElement.getAttribute("granule_option_id");
+      $(event.target).find("h4 select").val(granuleId).change();
+      revisionIdElement.style.top = 0;
+      revisionIdElement.style.left = 0;
+    }
+  });
+})
