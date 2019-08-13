7ec25c8426bad2ab0fe53dffc9946ebc6f83290e
Charles Huang <charles.huang@nasa.gov>
2019-08-13 15:02:58 -0400

    CMRARC-490: update role helper methods

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,10 +1,10 @@
 module SiteHelper
   def current_user_daac_records(records)
-    daac_records = if current_user.role.eql?("daac_curator")
-      records.daac(current_user.daac)
-    else
-      records
-    end
+    daac_records = if current_user.daac_curator?
+                     records.daac(current_user.daac)
+                   else
+                     records
+                   end
 
     records_sorted_by_short_name(daac_records)
   end
@@ -12,5 +12,4 @@ module SiteHelper
   def currator_feedback_records(records)
     records.joins(:record_datas, :reviews).where(record_data: { feedback: true}, reviews: { user_id: current_user.id })
   end
-  
 end
