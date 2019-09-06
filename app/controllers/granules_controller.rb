4a9daa7e508ddf2625afc15a277e2218f20074ad
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2019-09-06 12:13:56 -0400

    CMRARC-459 - added more error handling.

--- app/controllers/granules_controller.rb
+++ app/controllers/granules_controller.rb
@@ -115,15 +115,24 @@ class GranulesController < ApplicationController
   def ingest_specific
       collection = Collection.find(params[:id])
       authorize! :create_granule, collection
-      concept_id = params["granule_concept_id"]
+      collection_concept_id = collection.concept_id
+      granule_concept_id = params["granule_concept_id"]
+
+      record = collection.granules.find_by concept_id: granule_concept_id
+      unless record.nil?
+        flash[:notice] = "Sorry, granule review #{granule_concept_id} already exists!"
+        redirect_to collection_path(id: 1, record_id: collection.records.first.id)
+        return
+      end
+
       begin
-        Granule.ingest_specific(concept_id, current_user)
-        flash[:notice] = "Granule #{concept_id} ingested."
+        Granule.ingest_specific(collection_concept_id, granule_concept_id, current_user)
+        flash[:notice] = "Granule #{granule_concept_id} ingested."
       rescue Cmr::CmrError => e
-        flash[:notice] = "Sorry, granule #{concept_id} could not be found in CMR."
+        flash[:notice] = e.message
       rescue StandardError => e
-        flash[:notice] = "Sorry, granule #{concept_id} could not be ingested."
-        Rails.logger.error("Sorry, granule #{concept_id} could not be ingested.  error=#{e.message}")
+        flash[:notice] = "Sorry, granule #{granule_concept_id} could not be ingested."
+        Rails.logger.error("Sorry, granule #{granule_concept_id} could not be ingested.  error=#{e.message}")
       end
       redirect_to collection_path(id: 1, record_id: collection.records.first.id)
   end
