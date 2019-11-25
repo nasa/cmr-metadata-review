9b85aa1f7426e59f9cade08d631b46952ac34875
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2019-11-25 10:49:25 -0500

    CMRARC-369 changes to make feedback records show up.

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,20 +1,20 @@
 module ReviewsHelper
 
   def color_selected_list(color)
-    select_list = ["","","","","", ""]
+    select_list = ['','','','','', '']
     case color
-    when "green"
-      select_list[1] = "selected"
-    when "blue"
-      select_list[2] = "selected"
-    when "yellow"
-      select_list[3] = "selected"
-    when "red"
-      select_list[4] = "selected"
-    when "gray"
-      select_list[5] = "selected"
+    when 'green'
+      select_list[1] = 'selected'
+    when 'blue'
+      select_list[2] = 'selected'
+    when 'yellow'
+      select_list[3] = 'selected'
+    when 'red'
+      select_list[4] = 'selected'
+    when 'gray'
+      select_list[5] = 'selected'
     else
-      select_list[0] = "selected"
+      select_list[0] = 'selected'
     end
     select_list
   end
@@ -22,18 +22,18 @@ module ReviewsHelper
   #Platforms/Platform0/ShortName -> Platforms / Platform 0 / ShortName
   def split_on_capitals(title)
     #adds a space before the / and adds a space before digits
-    out_title = title.gsub("/", " / ")
+    out_title = title.gsub('/', ' / ')
     out_title = space_digits(out_title)
     out_title
   end
 
   def space_digits(title)
-    return title.gsub(/([0-9]+)/, " \\1")
+    return title.gsub(/([0-9]+)/, ' \\1')
   end
 
   def format_new_lines(string)
     unless string.nil?
-      return (raw string.gsub("\n", "<br>"))
+      return (raw string.gsub("\n", '<br>'))
     end
   end 
 
@@ -47,6 +47,14 @@ module ReviewsHelper
       values_hash[key] = value.gsub(URI.regexp(['http', 'https'])) { |url| "<a target=\"_blank\" href=\"#{url}\">#{url}</a>" }
     end
     values_hash
+  end
+  
+  def feedback_cell_class(current_user)
+    if current_user.daac_curator?
+      'feedback_cell review-toggle'
+    else
+      'feedback_cell review-toggle-disabled'
+    end
   end
 
 end
