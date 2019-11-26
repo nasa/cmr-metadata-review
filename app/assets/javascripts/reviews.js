fba4b60265a0257e97d87ced27ba5d2bcfa2895f
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2019-11-26 14:13:44 -0500

    CMRARC-369 more refactoring

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,23 +1,29 @@
-function feedbackClickHandler(cellId, checkId, pId, iconId, daac_curator) {
-    if (!daac_curator) {
-      return;
-    }
-    activateSaveButton();
+function feedbackClickHandler(e) {
+  title = $(e).attr("column_value");
+  cellId = "feedback_cell_" + title;
+  checkId = "feedback_check_" + title;
+  pId = "feedback_text_" + title;
+  iconId = "feedback_icon_" + title;
+  daac_curator = $(e).attr("daac_curator");
 
-    var checkbox = document.getElementById(checkId);
-    checkbox.checked = !checkbox.checked;
+  if (daac_curator == "false") {
+    return;
+  }
 
-    var iconClass = checkbox.checked ? 'eui-icon eui-check-o' : 'fa fa-comments';
-    document.getElementById(iconId).className = iconClass;
+  activateSaveButton();
+  var checkbox = document.getElementById(checkId);
+  checkbox.checked = !checkbox.checked;
+  var iconClass = checkbox.checked ? 'eui-icon eui-check-o' : 'fa fa-comments';
+  document.getElementById(iconId).className = iconClass;
 
-    var cellEl = document.getElementById(cellId), cellClass = "review-toggle__cell";
-    var textEl = document.getElementById(pId), textClass = "review-toggle__text";
+  var cellEl = document.getElementById(cellId), cellClass = "review-toggle__cell";
+  var textEl = document.getElementById(pId), textClass = "review-toggle__text";
 
-    if (checkbox.checked) {
-      cellEl.classList.add(cellClass);
-      textEl.classList.add(textClass);
-    } else {
-      cellEl.classList.remove(cellClass);
-      textEl.classList.remove(textClass);
-    }
+  if (checkbox.checked) {
+    cellEl.classList.add(cellClass);
+    textEl.classList.add(textClass);
+  } else {
+    cellEl.classList.remove(cellClass);
+    textEl.classList.remove(textClass);
+  }
 }
