610e65f3c219fdd85b28a707f4f9936d7fc59be4
Ryan Miller <ryan.miller-1@nasa.gov>
2020-03-02 10:00:23 -0500

    CMRARC-544 fixing e-mail previews

--- test/mailers/previews/daac_curator_mailer_preview.rb
+++ test/mailers/previews/daac_curator_mailer_preview.rb
@@ -7,6 +7,6 @@ class DaacCuratorMailerPreview < ActionMailer::Preview
     record2 = Record.new(id: 1_000_001, recordable_type: 'Granule', recordable_id: 1_000_001, revision_id: 1, released_to_daac_date: 1.days.ago)
     record2.record_datas = [RecordData.new(record_id: 1_000_001, column_name: 'GranuleUR', value: 'A really really really really really really really really really really really really really really really really really really really really really really really really long title')]
 
-    DaacCuratorMailer.released_records_digest_notification([user1, user2], Record.first(5), 'OB_DAAC')
+    DaacCuratorMailer.released_records_digest_notification([user1, user2], [record1, record2], 'OB_DAAC')
   end
 end
