7647450a00ed489804a8c77dec50216f7f53fb99
Ryan Miller <ryan.miller-1@nasa.gov>
2020-03-05 13:14:43 -0500

    CMRARC-544 updating schedule.rb to try to include the path to bundle.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,9 @@
 # Use this file to easily define all of your cron jobs.
-#
+
+ruby_path = File.expand_path('..', %x(which ruby))
+
+env :PATH, "#{ruby_path}:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin"
+
 set :output, 'log/session_cleanup_cron.log'
 every 1.day, at: '4:00 am' do
   rake 'sessions_cleanup_cron:cleanup'
