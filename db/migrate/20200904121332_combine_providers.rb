59a0f3d63bcd77c17727c58ef9bda146ebba8026
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2020-09-04 12:06:01 -0400

    CMRARC-592 migrates all records even deleted.

--- db/migrate/20200904121332_combine_providers.rb
+++ db/migrate/20200904121332_combine_providers.rb
@@ -1,6 +1,6 @@
 class CombineProviders < ActiveRecord::Migration[5.2]
   def up
-    Record.all.visible.where(daac: ["LARC_ASDC", "LARC"]).find_each do |record|
+    Record.where(daac: ["LARC_ASDC", "LARC"]).find_each do |record|
       prior_daac = record.daac
       success = record.update(daac: "ASDC")
       if success
@@ -9,7 +9,7 @@ class CombineProviders < ActiveRecord::Migration[5.2]
         Rails.logger.error "Could not migrate providers for ASDC from #{prior_daac} #{record.recordable_type}"
       end
     end
-    Record.all.visible.where(daac: ["NSIDCV0", "NSIDC_ECS"]).find_each do |record|
+    Record.where(daac: ["NSIDCV0", "NSIDC_ECS"]).find_each do |record|
       success = record.update(daac: "NSIDC")
       prior_daac = record.daac
       if success
@@ -18,7 +18,7 @@ class CombineProviders < ActiveRecord::Migration[5.2]
         Rails.logger.error "Could not migrate providers for NSIDC from #{prior_daac} #{record.recordable_type}"
       end
     end
-    Record.all.visible.where(daac: ["GHRC_CLOUD", "LANCEAMSR2"]).find_each do |record|
+    Record.where(daac: ["GHRC_CLOUD", "LANCEAMSR2"]).find_each do |record|
       success = record.update(daac: "GHRC")
       prior_daac = record.daac
       if success
@@ -30,7 +30,7 @@ class CombineProviders < ActiveRecord::Migration[5.2]
   end
 
   def down
-    Record.all.visible.where(daac: ["ASDC", "NSIDC", "GHRC"]).find_each do |record|
+    Record.where(daac: ["ASDC", "NSIDC", "GHRC"]).find_each do |record|
       daac = record.concept_id.partition('-').last
       prior_daac = record.daac
       success = record.update(daac: daac)
