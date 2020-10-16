48043e7d8bf2c1d682fee4656d162f459d8193fb
Hoan-Vu Tran-Ho <hoan.v.tranho@nasa.gov>
2020-10-16 15:21:15 -0400

    CMRARC-602 remove color filter

--- app/javascript/components/view/SearchView.js
+++ app/javascript/components/view/SearchView.js
@@ -22,7 +22,7 @@ const SearchView = observer(
                         <input onChange={() => {this.props.viewModel.filterByText(this.inputRef.current.value)}} ref={this.inputRef} type="text" className="eui-search-home" style={{ width: "240px", border: "solid 1px", borderColor: "lightgrey", marginRight: "4px", height: "30px" }} value={this.props.viewModel.filter} placeholder="Search" />
                         <button onClick={() => {this.props.viewModel.filterByText(this.inputRef.current.value)}} style={{ marginTop: "2px" }} type="button" className="eui-btn--sm eui-btn--green eui-search-home"><i className="eui-icon eui-fa-search"></i></button>
                     </div>
-                    <ColorFilterView viewModel={this.props.viewModel} section={this.props.section}/>
+                    {/*<ColorFilterView viewModel={this.props.viewModel} section={this.props.section}/>*/}
                   </div>
                 </React.Fragment>
             );
