db7e38d79ccf2bc54e00235e0193ea91e5530e73
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2021-02-24 17:13:48 -0500

    CMRARC-645 added db layer needed for invalid keywords.

---
+++ db/migrate/20210224213723_create_invalid_keywords.rb
@@ -0,0 +1,17 @@
+class CreateInvalidKeywords < ActiveRecord::Migration[5.2]
+  def change
+    create_table :invalid_keywords do |t|
+      t.datetime :last_updated
+      t.string :provider_id
+      t.string :concept_id
+      t.integer :revision_id
+      t.string :short_name
+      t.string :version
+      t.string :invalid_keyword_path
+      t.string :valid_keyword_path
+      t.string :ummc_field
+
+      t.timestamps
+    end
+  end
+end
