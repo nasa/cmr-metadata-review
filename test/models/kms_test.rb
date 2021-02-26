51aac4273e311b08766b620da5d1b3bbc690bcdc
Hoan-Vu Tran-Ho <hoan.v.tranho@nasa.gov>
2021-02-26 14:07:39 -0500

    CMRARC-662

--- test/models/kms_test.rb
+++ test/models/kms_test.rb
@@ -61,15 +61,15 @@ class KmsTest < ActiveSupport::TestCase
       assert_equal(true, result[key])
     end
 
-    # it "get kms base url" do
-    #   kms_base_url = @kms.get_kms_base_url
-    #   assert_equal('https://gcmd.sit.earthdata.nasa.gov', kms_base_url)
-    # end
-    #
-    # it "get kms url for science keywords" do
-    #   kms_url = @kms.get_kms_url('sciencekeywords')
-    #   assert_equal('https://gcmd.sit.earthdata.nasa.gov/kms/concepts/concept_scheme/sciencekeywords?format=csv', kms_url)
-    # end
+    it "get kms base url" do
+      kms_base_url = Kms.get_kms_base_url
+      assert_equal('https://gcmd.sit.earthdata.nasa.gov', kms_base_url)
+    end
+
+    it "get kms url for science keywords" do
+      kms_url = @kms.get_kms_url('sciencekeywords')
+      assert_equal('https://gcmd.sit.earthdata.nasa.gov/kms/concepts/concept_scheme/sciencekeywords?format=csv', kms_url)
+    end
 
   end
 end
