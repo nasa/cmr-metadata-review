3b593c3fbdebe9724dacc5916f081d1a8bf1da8e
Hoan-Vu Tran-Ho <hoan.v.tranho@nasa.gov>
2021-03-01 14:08:39 -0500

    CMRARC-664 Implement keyword validator and add test

---
+++ app/models/keyword_validator.rb
@@ -0,0 +1,50 @@
+class KeywordValidator
+
+  def initialize
+    super
+  end
+
+  def validate_keywords(providers = get_providers)
+    checker = KeywordChecker.new
+    updated_since = CmrSync.get_sync_date
+    providers.each do |provider|
+      concept_id_compound = CmrSync.get_concepts(provider, 2000, updated_since)
+      concept_ids = []
+      concept_id_compound.each do |cidc|
+        concept_ids << cidc[0]
+      end
+      success = false
+      InvalidKeyword.transaction do
+        begin
+          InvalidKeyword.remove_invalid_keywords(concept_ids)
+          concept_id_compound.each do |cidc|
+            concept_id = cidc[0]
+            revision_id = cidc[1]
+            short_name = cidc[2]
+            version_id = cidc[3]
+            doc = CmrSync.get_concept(concept_id, revision_id, 'umm_json')
+            KeywordChecker::SCHEMES.each do |scheme|
+              invalid_keywords = checker.get_invalid_keywords(doc, scheme)
+              ummc_field = checker.get_ummc_field(scheme)
+              invalid_keywords.each do |invalid_keyword|
+                invalid_keyword_model = InvalidKeyword.create_invalid_keyword(provider, concept_id, revision_id, short_name, version_id,
+                invalid_keyword, '', ummc_field)
+                invalid_keyword_model.save
+              end
+            end
+          end
+          success = true
+        rescue StandardError => e
+          Rails.logger.error("Error encountered validating keywords for provider #{provider}, #{e.message}")
+          success = false
+        end
+        # special case kind of exception: the database transaction will be rolled back, without passing on the exception.
+        raise ActiveRecord::Rollback, ' rollback Error reverting!' unless success
+      end
+    end
+  end
+
+  def get_providers
+    return ApplicationHelper::ARC_PROVIDERS
+  end
+end
---
+++ test/models/keyword_validator_test.rb
@@ -0,0 +1,73 @@
+require 'test_helper'
+require 'stub_data'
+Dir[Rails.root.join('test/**/*.rb')].each { |f| require f }
+
+class KeywordValidatorTest < ActiveSupport::TestCase
+  include Helpers::UserHelpers
+
+  TEST_PROVIDERS = %w[SCIOPS]
+
+  setup do
+    OmniAuth.config.test_mode = true
+    mock_login(role: 'arc_curator')
+
+    kms_base_url = Kms.get_kms_base_url
+    stub_header = {
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'User-Agent'=>'Ruby'
+    }
+    stub_request(:get, "#{kms_base_url}/kms/concepts/concept_scheme/sciencekeywords?format=csv").
+        with(headers: stub_header).
+        to_return(status: 200, body: get_stub('sciencekeywords.csv'), headers: {})
+
+    stub_request(:get, "#{kms_base_url}/kms/concepts/concept_scheme/platforms?format=csv").
+        with(headers: stub_header).
+        to_return(status: 200, body: get_stub('platforms.csv'), headers: {})
+
+    stub_request(:get, "#{kms_base_url}/kms/concepts/concept_scheme/instruments?format=csv").
+        with(headers: stub_header).
+        to_return(status: 200, body: get_stub('instruments.csv'), headers: {})
+
+    stub_request(:get, "#{kms_base_url}/kms/concepts/concept_scheme/projects?format=csv").
+        with(headers: stub_header).
+        to_return(status: 200, body: get_stub('projects.csv'), headers: {})
+
+    stub_request(:get, "#{kms_base_url}/kms/concepts/concept_scheme/providers?format=csv").
+        with(headers: stub_header).
+        to_return(status: 200, body: get_stub('providers.csv'), headers: {})
+
+    stub_request(:get, "#{kms_base_url}/kms/concepts/concept_scheme/ProductLevelId?format=csv").
+        with(headers: stub_header).
+        to_return(status: 200, body: get_stub('ProductLevelId.csv'), headers: {})
+
+    stub_request(:get, "#{kms_base_url}/kms/concepts/concept_scheme/GranuleDataFormat?format=csv").
+        with(headers: stub_header).
+        to_return(status: 200, body: get_stub('granuledataformat.csv'), headers: {})
+
+    cmr_base_url = Cmr.get_cmr_base_url
+    stub_header_faraday = {
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'User-Agent'=>'Faraday v0.15.3'
+    }
+    stub_request(:get, "#{cmr_base_url}/search/collections.umm-json?page_num=1&page_size=2000&provider=SCIOPS&updated_since=1971-01-01T07%3A00%3A00-05%3A00").
+        with(headers: stub_header_faraday).
+        to_return(status: 200, body: get_stub('keyword_validator_get_concepts.json'), headers: {})
+
+    stub_request(:get, "#{cmr_base_url}/search/concepts/C1200207458-SCIOPS/1.umm_json").
+        with(headers: stub_header).
+        to_return(status: 200, body: get_stub('keyword_validator_get_concept_C1200207458-SCIOPS.json'), headers: {})
+
+    stub_request(:get, "#{cmr_base_url}/search/concepts/C1200208767-SCIOPS/1.umm_json").
+        with(headers: stub_header).
+        to_return(status: 200, body: get_stub('keyword_validator_get_concept_C1200208767-SCIOPS.json'), headers: {})
+  end
+  describe 'keyword validator test' do
+    it 'validate keyword' do
+      keyword_validator = KeywordValidator.new
+      keyword_validator.validate_keywords(TEST_PROVIDERS)
+      assert_equal(InvalidKeyword.all.length, 9)
+    end
+  end
+end
