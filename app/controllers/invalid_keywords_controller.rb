ee0194924cca5e6da1c1ad2d888c21dcc47e077f
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2021-03-02 17:30:36 -0500

    CMRARC-647 hooked up controller, added tests.

--- app/controllers/invalid_keywords_controller.rb
+++ app/controllers/invalid_keywords_controller.rb
@@ -1,6 +1,10 @@
 class InvalidKeywordsController < ApplicationController
   respond_to :csv
   def csv_report
-    render csv: InvalidKeyword.all
+    @records = InvalidKeyword.get_invalid_keywords(params["provider"])
+    respond_to do |format|
+      format.html
+      format.csv { send_data @records.to_csv, filename: "invalid_keywords-#{Date.today}.csv" }
+    end
   end
 end
