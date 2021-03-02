26d01b801499efed064080f56086868071f2e0b0
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2021-03-02 16:57:33 -0500

    CMRARC-647 fixed some issues with keyword validator.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,12 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.find_each do |model|
+        csv << model.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_26_114050) do
+ActiveRecord::Schema.define(version: 2021_03_02_195108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
