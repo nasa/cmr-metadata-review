bb36938506aedf6c42aacb94cbe351626f6b1898
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2021-03-04 16:55:12 -0500

    CMRARC-665 access kms prod only now.

--- app/models/kms.rb
+++ app/models/kms.rb
@@ -86,10 +86,12 @@ class Kms
   end
 
   def self.get_kms_base_url
-    kms_base_url = Rails.application.config.kms_base_url
-    if kms_base_url.nil?
-      kms_base_url = 'https://gcmd.earthdata.nasa.gov'
-    end
+    # commenting this out because mmt hardware can't access gcmd earthdata on sit.
+    # kms_base_url = Rails.application.config.kms_base_url
+    # if kms_base_url.nil?
+    #   kms_base_url = 'https://gcmd.earthdata.nasa.gov'
+    # end
+    kms_base_url = 'https://gcmd.earthdata.nasa.gov'
     kms_base_url
   end
 
