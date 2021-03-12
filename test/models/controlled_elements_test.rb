d18097c7549422ca3321a44ef9b434a00318152e
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2021-03-11 20:59:23 -0500

    CMRARC-648 changes for echo10

--- test/models/controlled_elements_test.rb
+++ test/models/controlled_elements_test.rb
@@ -9,7 +9,6 @@ class ControlledElementsTest < ActiveSupport::TestCase
       value = map["ArchiveAndDistributionInformation/FileArchiveInformation/Format"]
       assert_includes(value,'GCMD Keywords https://gcmd.earthdata.nasa.gov/kms/concepts/concept_scheme/GranuleDataFormat/?format=csv')
 
-      controlled_elements.export_controlled_elements_to_csv('dif10')
     end
 
     it "can return proper description given specified field for dif10" do
@@ -17,7 +16,14 @@ class ControlledElementsTest < ActiveSupport::TestCase
       map = controlled_elements.mapping("dif10")
       value = map["Location/Location_Category"]
       assert_includes(value,'Only values from the "Location_Category" column')
-      controlled_elements.export_controlled_elements_to_csv('dif10')
+      controlled_elements.export_controlled_elements_to_csv('echo10')
+    end
+
+    it "can return proper description given specified field for echo10" do
+      controlled_elements = ControlledElements.instance
+      map = controlled_elements.mapping("echo10")
+      value = map["DOI/MissingReason"]
+      assert_includes(value,'ECHO metadata common schema MissingReason ["Unknown"]')
     end
 
   end
