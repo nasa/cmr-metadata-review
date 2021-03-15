5bb2b952b8ac418f7da4beb4fab27af894f9278b
Hoan-Vu Tran-Ho <hoan.v.tranho@nasa.gov>
2021-03-15 15:49:18 -0400

    CMRARC-666 Update get keyword recommendations

--- app/models/keyword_validator.rb
+++ app/models/keyword_validator.rb
@@ -13,13 +13,8 @@ class KeywordValidator
     else
       updated_since = CmrSync.get_sync_date
     end
-
     updated_now = DateTime.now
     records_processed = 0
-    invalid_keywords_per_scheme = {}
-    KeywordChecker::SCHEMES.each do |scheme|
-      invalid_keywords_per_scheme[scheme] = Set.new
-    end
     providers.each do |provider|
       Rails.logger.info "Retrieving concepts for #{provider} using #{updated_since.nil? ? "" : updated_since.utc.iso8601}"
       concept_id_compound = CmrSync.get_concepts(provider, 2000, updated_since)
@@ -37,34 +32,37 @@ class KeywordValidator
           short_name = cidc[2]
           version_id = cidc[3]
           Rails.logger.info("Processing invalid keywords for #{concept_id} -- #{provider} ")
-          doc = CmrSync.get_concept(concept_id, revision_id, 'umm_json')
-          KeywordChecker::SCHEMES.each do |scheme|
-            invalid_keywords = checker.get_invalid_keywords(doc, scheme)
-            invalid_keywords_per_scheme[scheme] + invalid_keywords
-            ummc_field = checker.get_ummc_field(scheme)
-            invalid_keywords.each do |invalid_keyword|
-              invalid_keyword_model = InvalidKeyword.create_invalid_keyword(provider, concept_id, revision_id, short_name, version_id,
-              invalid_keyword, nil, ummc_field)
-              invalid_keyword_model.save!
+          begin
+            doc = CmrSync.get_concept(concept_id, revision_id, 'umm_json')
+            KeywordChecker::SCHEMES.each do |scheme|
+              invalid_keywords = checker.get_invalid_keywords(doc, scheme)
+              ummc_field = checker.get_ummc_field(scheme)
+              invalid_keywords.each do |invalid_keyword|
+                invalid_keyword_model = InvalidKeyword.create_invalid_keyword(provider, scheme, concept_id, revision_id, short_name, version_id,
+                invalid_keyword, nil, ummc_field)
+                invalid_keyword_model.save!
+              end
             end
+          rescue => e
+            Rails.logger.error("Failed assigning invalid keywords to concept id=#{concept_id}, message=#{e.message}")
           end
-          sleep 0.05 # 600 concepts per minute
+          sleep 0.1 # 600 concepts per minute
         end
       end
     end
-    update_recommended_keywords(invalid_keywords_per_scheme)
+    update_recommended_keywords
     CmrSync.update_sync_date(updated_now)
     records_processed
   end
 
-  def self.update_recommended_keywords(invalid_keywords_per_scheme)
-    invalid_keywords_per_scheme.keys.each do |scheme|
-      invalid_keywords = invalid_keywords_per_scheme[scheme]
-      recommendations = Kms.new.get_recommended_keywords(invalid_keywords, scheme)
-      recommendations.keys.each do |invalid_keyword|
-        map_value = recommendations[invalid_keyword]
-        recommended_keyword = (map_value.nil? || map_value.downcase == 'deleted') ? '' : map_value
-        db_invalid_keywords = InvalidKeyword.where(invalid_keyword:invalid_keyword)
+  def self.update_recommended_keywords
+    KeywordChecker::SCHEMES.each do |scheme|
+      invalid_keywords = InvalidKeyword.where(scheme:scheme)
+      keywords = invalid_keywords.map(&:invalid_keyword_path)
+      recommendations = Kms.new.get_recommended_keywords(keywords, scheme)
+      recommendations.each do |invalid_keyword, recommended_keyword|
+        recommended_keyword = (recommended_keyword.nil? || recommended_keyword.downcase == 'deleted') ? '' : recommended_keyword
+        db_invalid_keywords = InvalidKeyword.where(invalid_keyword_path:invalid_keyword)
         db_invalid_keywords.each do |db_invalid_keyword|
           db_invalid_keyword.valid_keyword_path = recommended_keyword
           db_invalid_keyword.save!
