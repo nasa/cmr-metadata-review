7067aace6b684c2522c0bb11867ca992c594e454
Larry Gilliam <lgilliam@innovim.com>
2021-06-01 01:55:07 -0400

    remove check for json truthyness

--- app/javascript/components/model/Result.js
+++ app/javascript/components/model/Result.js
@@ -9,18 +9,17 @@ export default class Result {
     this.itemsPerPage = null // Int
     this.records = [] // [String:Any]
 
-    if(json) {
-      this.totalItems = json["total_count"]
-      this.currentPage = json["page_num"];
-      this.itemsPerPage = json["page_size"]
-      this.records.length = 0;
+    this.totalItems = json["total_count"]
+    this.currentPage = json["page_num"];
+    this.itemsPerPage = json["page_size"]
+    this.records.length = 0;
 
-      let records = json["records"]
-      for (let i=0; i<records.length; i++) {
-        records[i].no = (this.itemsPerPage*(this.currentPage-1))+(i+1)
-      }
-      this.records.push(...records)
+    let records = json["records"]
+    for (let i=0; i<records.length; i++) {
+      records[i].no = (this.itemsPerPage*(this.currentPage-1))+(i+1)
     }
+    this.records.push(...records)
+
   }
 
   get totalPages() {
