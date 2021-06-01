7e552c80e618f852b2efd9ed9e9b746f8e622ae4
Larry Gilliam <lgilliam@innovim.com>
2021-06-01 06:12:01 -0400

    put back mailer settings in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,8 +33,11 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :file
+  config.action_mailer.file_settings = {
+    location: 'log/emails'
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
