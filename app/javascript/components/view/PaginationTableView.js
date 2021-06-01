6b78d7abf7a43ba7c2705a2fe382f5e7fc5b3f11
Larry Gilliam <lgilliam@innovim.com>
2021-06-01 01:56:34 -0400

    remove comment

--- app/javascript/components/view/PaginationTableView.js
+++ app/javascript/components/view/PaginationTableView.js
@@ -9,13 +9,6 @@ import "../eui.css"
 import "../pagination.css"
 import ColorFilterView from "./ColorFilterView";
 
-// class PaginationTableView extends React.Component {
-//   viewModel = 'x'
-//   render() {
-//     return <h1>Hello, World!</h1>
-//   }
-// }
-
 const PaginationTableView = observer(
   class PaginationTableView extends React.Component {
 
