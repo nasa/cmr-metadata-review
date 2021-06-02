362db7e112c2e81611b384bc604f9914183f97ac
Jose Rivas <jose.rivas.0410@gmail.com>
2021-06-02 14:14:36 -0500

    CMRARC-713
    writing out the test for searching up collections under different providers

---
+++ test/features/can_search_virtual_providers_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+Dir[Rails.root.join('test/**/*.rb')].each { |f| require f }
+
+class CanSearchVirtualProvidersTest < Capybara::Rails::TestCase
+  include Helpers::UserHelpers
+  include Helpers::ReviewsHelper
+  include Helpers::HomeHelper
+  include Helpers::CollectionsHelper
+
+  before do
+    OmniAuth.config.test_mode = true
+    mock_login(role: 'admin')
+
+    stub_request(:get, "#{Cmr.get_cmr_base_url}/search/granules.echo10?concept_id=G309210-GHRC")
+        .with(
+            headers: {'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Accept' => '*/*', 'User-Agent' => 'Ruby'}
+        )
+        .to_return(status: 200, body: '<?xml version="1.0" encoding="UTF-8"?><results><hits>0</hits><took>32</took></results>', headers: {})
+    end
+
+    describe "GET #search" do
+      it "searches for collections under ASDC provider" do
+        visit '/home'
+        find("#provider").click
+        click_on '#provider > option:nth-child(3)'
+      end
+    end
+end
