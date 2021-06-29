561e08a39908e87c9a1b4551998b0e578e0b5945
Jose Rivas <jose.rivas.0410@gmail.com>
2021-06-29 17:17:20 -0500

    CMRARC-711
    Got the stub request and will now build the srcipt

--- test/models/collection_test.rb
+++ test/models/collection_test.rb
@@ -15,4 +15,20 @@ class RecordTest < ActiveSupport::TestCase
       assert_equal(data_format, "echo10")
     end
   end
+
+  describe "GET #record" do
+    it "creates a new record by url" do
+      stub_request(:get, "https://cmr.earthdata.nasa.gov/search/concepts/C1652975935-PODAAC.echo10")
+          .with(
+            headers: {
+              'Accept' => '*/*',
+              'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+              'User-Agent' => 'Ruby'
+            }
+          )
+          .to_return(status: 200, body: get_stub('search_concepts_C1652975935PODAAC_echo10.xml'), headers: {'content-type': 'application/echo10+xml; charset=utf-8'})
+
+          
+    end
+  end
 end
