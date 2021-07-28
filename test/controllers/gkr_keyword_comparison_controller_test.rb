824f505b6e0a22bd71db8c77482d21f172ba6efd
Hanna Choe <hannachoes@gmail.com>
2021-07-28 10:26:16 -0400

    CMRARC-730: deleted unneccessary files

--- test/controllers/gkr_keyword_comparison_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class GkrKeywordComparisonControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get gkr_keyword_comparison_show_url
-    assert_response :success
-  end
-
-end
