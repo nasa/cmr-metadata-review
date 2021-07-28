b5ac7f6205fa3d69688725ad880b9c2862877dca
Hanna Choe <hannachoes@gmail.com>
2021-07-28 10:51:05 -0400

    CMRARC-730: removed byebug and save_and_open_screenshot

--- app/controllers/gkr_keyword_comparison_controller.rb
+++ app/controllers/gkr_keyword_comparison_controller.rb
@@ -6,10 +6,8 @@ class GkrKeywordComparisonController < ApplicationController
   def create
     concept_id = params[:concept_id]
     threshold = params[:threshold]
-    collection = Cmr.get_raw_collection(concept_id, 'umm_json') # using this method bc get_concept() did not work for all concept ids
+    collection = Cmr.get_raw_collection(concept_id, 'umm_json')
     abstract_p = collection['Abstract']
-
-    byebug
  
     redirect_to gkr_keyword_comparison_path(abstract: abstract_p)
   end
--- test/features/gkr_keyword_comparison_test.rb
+++ test/features/gkr_keyword_comparison_test.rb
@@ -16,7 +16,7 @@ class GkrKeywordComparisonTest < SystemTestCase
               'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
               'User-Agent'=>'Ruby'
         }).
-      to_return(status: 200, body: get_stub('test.json'), headers: {'content-type': 'application/vnd.nasa.cmr.umm_results+json;version=1.16.5; charset=utf-8'})
+      to_return(status: 200, body: get_stub('C1200400842-GHRC.json'), headers: {'content-type': 'application/vnd.nasa.cmr.umm_results+json;version=1.16.5; charset=utf-8'})
     end
 
     it 'should render page, grab parameters and retrieve appropriate collection' do
@@ -28,7 +28,6 @@ class GkrKeywordComparisonTest < SystemTestCase
       fill_in 'Threshold', with: '1'
       click_on 'Submit'
 
-      save_and_open_screenshot
       assert has_content?('The National Center for Environmental Prediction/Aviation Weather Center Infrared Global Geostationary Composite data set contains global composite images from the infrared channels of multiple weather satellites in geosynchronous orbit. These satellites include the GMS (Japan), GOES (USA), and Meteosat (Europe). These imagers span nearly the entire globe with a small gap over India. The data resolution is 14 km spatial with the data remapped into a Mercator projection. The data have not necessarily been cross calibrated between sensors.')
     end
   end
