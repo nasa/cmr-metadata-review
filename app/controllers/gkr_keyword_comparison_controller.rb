1988491cde1e9dbe435b4afcd89afa033d71eade
Hanna Choe <hannachoes@gmail.com>
2021-07-29 11:32:38 -0400

    CMRARC-731: grab keywords and successfully display them on application

--- app/controllers/gkr_keyword_comparison_controller.rb
+++ app/controllers/gkr_keyword_comparison_controller.rb
@@ -1,6 +1,7 @@
 class GkrKeywordComparisonController < ApplicationController
   def show
     @abstract = params[:abstract]
+    @cmr_keywords = params[:cmr_keywords]
   end
 
   def create
@@ -8,7 +9,8 @@ class GkrKeywordComparisonController < ApplicationController
     threshold = params[:threshold]
     collection = Cmr.get_raw_collection(concept_id, 'umm_json')
     abstract_p = collection['Abstract']
- 
-    redirect_to gkr_keyword_comparison_path(abstract: abstract_p)
+    cmr_keywords_p = collection['ScienceKeywords']
+
+    redirect_to gkr_keyword_comparison_path(abstract: abstract_p, cmr_keywords: cmr_keywords_p)
   end
 end
