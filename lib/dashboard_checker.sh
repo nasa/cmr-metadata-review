7c0c69ccc1c8a4194e3331a38def50e5d7e9a1be
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2021-08-10 15:02:49 -0400

    CMRARC-736: redirects stdout and error to /tmp/python3_output.txt

--- lib/dashboard_checker.sh
+++ lib/dashboard_checker.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-if [ -n "$bamboo_RH_PYTHON3_ENABLE" ]; then
-  source /opt/rh/rh-python38/enable
-fi
+source /opt/rh/rh-python38/enable
 python3 -W ignore lib/dashboard_checker.py $1 $2 2>&1 | tee /tmp/python3_output.txt
