dc96bb53e58392209f6e813509d52d05e9a9d9bc
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2021-08-10 13:55:55 -0400

    CMRARC-736: Changes to how it sources python-38

--- lib/dashboard_checker.sh
+++ lib/dashboard_checker.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+python3 --version > '/tmp/prior_python_version.txt'
 if [ -n "$bamboo_RH_PYTHON3_ENABLE" ]; then
-  source ${bamboo_RH_PYTHON3_ENABLE}
+  source /opt/rh/rh-python38/enable
 fi
+python3 --version > '/tmp/new_python_version.txt'
 python3 -W ignore lib/dashboard_checker.py $1 $2
