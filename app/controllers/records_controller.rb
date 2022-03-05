94b412475e68e2bd8a3c48a8e0d86f89edde2cea
Chris Gokey <chrisgokey@gmail.com>
2022-03-05 13:53:02 -0500

    CMRARC-602: Removed comments, now uses union, and fixed an issue with empty array.

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -68,10 +68,7 @@ class RecordsController < ApplicationController
       " records.revision_id, collections.short_name, ingests.date_ingested" + query
 
     if (color_code)
-      # response_records = Record.find_by_sql(records_query)
       response_record_ids = ActiveRecord::Base.connection.exec_query(records_query).rows.map {|r|r[0]}
-      # select distinct records.id from records, records_data where records.id == record_data.record_id and record_data.color = color
-      # and records.id in (#{response_records.map(&:id).join(",")})
       response_record_ids = filter_by_color_code(response_record_ids, color_code, color_code_filter_collection, color_code_filter_granule)
       if response_record_ids.count == 0
         result = {total_count: 0, page_num: page_num, page_size: page_size, records: []}
@@ -122,18 +119,23 @@ class RecordsController < ApplicationController
 
       # retrieve all collection records with 'color_code'
       if color_code_filter_collection
-        collection_response_records = ActiveRecord::Base.connection.exec_query("select distinct records.id, records.format  from records, record_data where records.id = record_data.record_id and record_data.color = '#{color_code}' and (records.state != 'closed' and records.state != 'finished') and records.recordable_type = 'Collection' and records.id in (#{response_record_ids.join(",")})").rows.map {|array|array[0]}
+        if response_record_ids.empty?
+          collection_response_records = ActiveRecord::Base.connection.exec_query("select distinct records.id, records.format  from records, record_data where records.id = record_data.record_id and record_data.color = '#{color_code}' and (records.state != 'closed' and records.state != 'finished') and records.recordable_type = 'Collection'").rows.map {|array|array[0]}
+        else
+          collection_response_records = ActiveRecord::Base.connection.exec_query("select distinct records.id, records.format  from records, record_data where records.id = record_data.record_id and record_data.color = '#{color_code}' and (records.state != 'closed' and records.state != 'finished') and records.recordable_type = 'Collection' and records.id in (#{response_record_ids.join(",")})").rows.map {|array|array[0]}
+        end
       end
       # retrieve all collection records where it has granules with 'color_code'
       if color_code_filter_granule
-        granule_response_records = ActiveRecord::Base.connection.exec_query("select distinct records.id, records.format from records, record_data where records.id = record_data.record_id and record_data.color = '#{color_code}' and (records.state != 'closed' and records.state != 'finished') and records.recordable_type = 'Granule' and records.id in (#{response_record_ids.join(",")})").rows.map {|array|array[0]}
+        if response_record_ids.empty?
+          granule_response_records = ActiveRecord::Base.connection.exec_query("select distinct records.id, records.format from records, record_data where records.id = record_data.record_id and record_data.color = '#{color_code}' and (records.state != 'closed' and records.state != 'finished') and records.recordable_type = 'Granule'").rows.map {|array|array[0]}
+        else
+          granule_response_records = ActiveRecord::Base.connection.exec_query("select distinct records.id, records.format from records, record_data where records.id = record_data.record_id and record_data.color = '#{color_code}' and (records.state != 'closed' and records.state != 'finished') and records.recordable_type = 'Granule' and records.id in (#{response_record_ids.join(",")})").rows.map {|array|array[0]}
+        end
       end
 
       # only include records that are in either collection_records OR granule_records
-      response_record_ids.select! { |record_id|
-        collection_response_records.select { |c_record_id| c_record_id == record_id }.count > 0 ||
-          granule_response_records.select { |g_record_id| g_record_id == record_id }.count > 0
-      }
+      response_record_ids = collection_response_records.union(granule_response_records)
     end
     response_record_ids
   end
