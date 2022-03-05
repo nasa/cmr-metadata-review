e4819c93b61ecbf2d1d329daf67d5d2b921cdafd
Chris Gokey <chrisgokey@gmail.com>
2022-03-05 13:21:36 -0500

    CMRARC-753: Added desired fields, required fields, section titles, and model object for umm-g.

---
+++ app/models/record_formats/ummg_record.rb
@@ -0,0 +1,32 @@
+# RecordFormats are modules to be included in record objects upon initialization
+#
+# Since individual record formats have different underlying structures of RecordData objects
+# unique accessors are needed for each format to access commonly requested data.
+module RecordFormats
+  module UmmGRecord
+    def get_section_titles
+      SectionTitles.instance.get_format_fields('ummg')
+    end
+
+    def field_required?(field)
+      RequiredFields.instance.get_format_fields('ummg').include?(field)
+    end
+
+    # There is no script yet for UMM collections
+    def create_script(raw_data = nil)
+      nil
+    end
+
+    def evaluate_script(raw_data = nil)
+      {}
+    end
+
+    def controlled_element_map
+      ControlledElements.instance.mapping("ummg_json")
+    end
+
+    def long_name_field
+      LONG_NAME_FIELD
+    end
+  end
+end
