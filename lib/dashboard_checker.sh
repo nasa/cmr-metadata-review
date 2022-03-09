da45bd25bed9dc370d7fe73af5d5838ef959a6e0
Chris Gokey <chrisgokey@gmail.com>
2022-03-09 12:24:51 -0500

    CMRARC-762: Added output to /tmp to help debug in SIT.

--- lib/dashboard_checker.sh
+++ lib/dashboard_checker.sh
@@ -2,4 +2,8 @@
 #if [ -d "/opt/rh/rh-python38" ]; then
 #  source /opt/rh/rh-python38/enable
 #fi
-python3 -W ignore lib/dashboard_checker.py $1 $2
+cat $1 > /tmp/record.txt
+echo "python3 -W ignore lib/dashboard_checker.py $1 $2" > /tmp/command.txt
+python3 -W ignore lib/dashboard_checker.py $1 $2 > /tmp/output.txt
+cat /tmp/output.txt
+
