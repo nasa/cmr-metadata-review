1204aac9fc45ac7bcf169b99565ab63afc2d656d
Christopher Gokey <christopher.d.gokey@nasa.gov>
2022-04-26 17:55:03 -0400

    CMRARC-772: Updated required/desired fields list for all lists.

---
+++ data/move.sh
@@ -0,0 +1,27 @@
+mv "CMR Curation Dashboard - DIF10 Desired Fields.csv" dif10_desired_fields.csv
+mv "CMR Curation Dashboard - DIF10 Required Fields.csv" dif10_required_fields.csv
+mv "CMR Curation Dashboard - DIF10.2 Controlled Vocab Definitions.csv" controlled_elements_dif10.csv
+mv "CMR Curation Dashboard - ECHO10 Controlled Vocab Definitions.csv" controlled_elements_echo10.csv
+mv "CMR Curation Dashboard - ECHO10 Required Fields.csv" echo10_required_fields.csv
+mv "CMR Curation Dashboard - ECHO10 Section Titles.csv" echo10_section_titles.csv
+mv "CMR Curation Dashboard - ECHO10-Granule Required Fields.csv" echo10_granule_required_fields.csv
+mv "CMR Curation Dashboard - ECHO10-Granule Section List.csv" echo10_granule_section_titles.csv
+mv "CMR Curation Dashboard - ISO-MENDS Mapping.csv" ummc_to_iso-mends_mapping.csv
+mv "CMR Curation Dashboard - ISO-SMAP Mapping.csv" ummc_to_iso-smap_mapping.csv
+mv "CMR Curation Dashboard - UMM-C Controlled Vocab Definitions.csv" controlled_elements_umm_json.csv
+mv "CMR Curation Dashboard - UMM-C Desired Fields.csv" ummc_desired_fields.csv
+mv "CMR Curation Dashboard - UMM-C Required Fields.csv" ummc_required_fields.csv
+mv "CMR Curation Dashboard - UMM-C Section Titles.csv" ummc_section_titles.csv
+mv "CMR Curation Dashboard - UMM-G Controlled Vocab Definitions.csv" controlled_elements_umm-g.csv
+mv "CMR Curation Dashboard - UMM-G Desired Fields.csv" ummg_desired_fields.csv
+mv "CMR Curation Dashboard - UMM-G Required Fields.csv" ummg_required_fields.csv
+mv "CMR Curation Dashboard - UMM-G Section Titles.csv" ummg_section_titles.csv
+mv "CMR Curation Dashboard - DIF10 Section Titles.csv" dif10_section_titles.csv
+mv "CMR Curation Dashboard - ECHO10 Desired Fields.csv" echo10_desired_fields.csv
+mv "CMR Curation Dashboard - ECHO10-Granule Desired Fields.csv" echo10_granule_desired_fields.csv
+
+
+
+
+
+
