eb98fc1512968a81c5be53bcb53b0fc7b6353b16
Christopher Gokey <christopher.d.gokey@nasa.gov>
2022-11-03 17:18:45 -0400

    CMRARC-784: fix to python path.

--- lib/granule_checker.sh
+++ lib/granule_checker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-#if [ -d "/opt/rh/rh-python38" ]; then
-#  source /opt/rh/rh-python38/enable
-#fi
+if [ -d "/opt/rh/rh-python38" ]; then
+  source /opt/rh/rh-python38/enable
+fi
 #pyenv local 2.7.18
 python2 -W ignore lib/GranuleChecker.py $1
 
