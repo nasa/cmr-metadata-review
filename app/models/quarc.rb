c417cf8f1da4fc97a4ce494cb3893346fd61930a
Christopher Gokey <christopher.d.gokey@nasa.gov>
2022-11-30 12:25:30 -0500

    CMRARC-789: Added comments to the quarc class.

--- app/models/quarc.rb
+++ app/models/quarc.rb
@@ -40,13 +40,24 @@ class Quarc
     return path
   end
 
+  # Given the specified path and the check being applied, include the check result of the value/message to the "result" dictionary.
+  # If the check is valid (no errors), just include "OK; "
+  #
+  # e.g. check_data = { "valid": false, "value": [ 43 ],
+  # "message": [ "Warning: The abstract provided may be inadequate based on length." ],
+  # "remediation": "Provide a more comprehensive description, mimicking a journal abstract that is useful to the science
+  # community but also approachable for a first time user of the data." }
   def assign_results(path, check, check_data, result)
     if check_data.has_key?("valid")
       valid = check_data["valid"]
+      # if check is valid, use "OK"
       if valid
         result[path] += "OK; " if result[path] === ""
       else
+        # prior check said this was OK, but this check says not.
         result[path] == "" if result[path] == "OK; "
+
+        # Use the message if we have one.
         if check_data.has_key?("message")
           result[path] += "" "<b>Errors:</b><ul>"
           check_data["message"].each do |message|
@@ -57,12 +68,16 @@ class Quarc
         if check_data.has_key?("remediation")
           result[path] += "<b>Remediation:</b><br>" + check_data["remediation"]
         else
+          # Otherwise just mention the check failed.
           result[path] += check + " failed<br>"
         end
       end
     end
   end
 
+  # This just cleans up the result path, it will remove a trailing ; and if the result path's value == "" then will remove it altogher from the
+  # dictionary, hence all checks passed.
+  # e.g., result[path] = "OK; " will return "OK"
   def trim_results_path(results, path)
     if results[path].end_with?("; ")
       count = results[path].length - 2
@@ -73,6 +88,9 @@ class Quarc
     end
   end
 
+  # Main logic that parses through the errors for the specified field path and assigns the results of the checks to the "result" dictionary.
+  # The "result" dictionary is ["path":"result1;result2;"]
+  # e.g. see arc_response.json (in this directory) for an example of what errors looks like.
   def parse_checks(field_path, errors, result)
     path = remove_doctype(field_path)
     result[path] = ""
