058b39337e96add225eab92fe2260a5693b525c0
tranhohoanvu <hoan.v.tranho@nasa.gov>
2022-12-22 10:50:42 -0500

    CMRARC-779: Consider case of nil as value and has key

--- app/models/quarc.rb
+++ app/models/quarc.rb
@@ -71,7 +71,7 @@ class Quarc
           end
           result[path] += "</ul>"
         end
-        if check_data.has_key?("remediation") && check_data["remediation"]
+        if check_data["remediation"]
           result[path] += "<b>Remediation:</b><br>" + check_data["remediation"]
         else
           # Otherwise just mention the check failed.
