8556db0f4c5cb99bfe70f4cc6ea03f072c24af6e
Christopher D. Gokey <chrisgokey@bitbucket.org>
2024-05-06 17:38:47 -0400

    CMRARC-850: Added shakapacker to rails 7.

--- config/environments/uat.rb
+++ config/environments/uat.rb
@@ -25,8 +25,8 @@ Rails.application.configure do
   config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  # config.assets.js_compressor = :uglifier
+  # # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
