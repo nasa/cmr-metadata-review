e7bbd382ab4116126dc16594814faac4b37626fc
Christopher D. Gokey <chrisgokey@bitbucket.org>
2024-06-25 16:46:55 -0400

    CMRARC-861: Added json to retrieve metadata and review data from review.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,6 +4,66 @@ class ReviewsController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_curation
 
+  def get_raw_metadata(concept_id, revision_id, format)
+    url = "#{Cmr.get_cmr_base_url}/search/concepts/#{concept_id}#{revision_id.nil? ? "" : "/#{revision_id}"}#{format.nil? ? "" : ".#{format}"}"
+    Cmr.cmr_request(url).body
+  end
+
+  def json
+    @record = Record.find_by id: params[:id]
+
+    @marked_done = @record.closed?
+
+    @collection_record = Record.find_by id: params[:id]
+
+    @navigation_list = @record.sections.map {|section| section[0] }
+
+    @script_comment = @collection_record.get_script_comments
+
+    @justification_discussions = @record.discussions.justification
+    @feedback_discussions = @record.discussions.feedback
+
+    section_index = params["section_index"].to_i
+    section = @record.sections[section_index];
+
+    @section_title = section[0]
+    @section_titles = section[1]
+    @controlled_notices = @record.controlled_notice_list(@section_titles)
+
+    @bubble_data = []
+    bubble_map = @record.bubble_map
+    @section_titles.each do |title|
+      unless bubble_map[title].nil?
+        @bubble_data.push(bubble_map[title])
+      end
+    end
+
+    @flagged_by_script = @record.binary_script_values
+    @script_values = @record.script_values
+    @script_values = replace_links(@script_values)
+
+    @previous_values = replace_links(@previous_values)
+
+    @previous_recommendations = @record.previous_recommendations
+
+    @current_values = @record.values
+    @current_values = replace_links(@current_values)
+
+    @recommendations = @record.get_recommendations
+    @second_opinions = @record.get_opinions
+    @feedbacks = @record.get_feedbacks
+
+    @color_codes = @record.color_codes
+    metadata = JSON.parse(get_raw_metadata(@record.recordable.concept_id, @record.revision_id, @record.format))
+    review_data = @record.record_datas
+    result = { metadata: metadata, review_data: review_data  }
+    render json: result
+  end
+
+  def save_review
+    
+  end
+
   def show
     @record = Record.find_by id: params[:id]
 
